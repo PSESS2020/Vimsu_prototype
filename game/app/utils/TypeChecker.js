694b841e5f6710b3b785c1d82b2269b3b8a135cf
pnschumacher <philipp.schumacher@gmx.de>
2020-06-30 15:09:04 +0200

    Fixed TypeChecker.js export, static methods

--- game/app/utils/TypeChecker.js
+++ game/app/utils/TypeChecker.js
@@ -1,70 +1,74 @@
-export function isValidString(value) 
-{
-    if ((typeof value !== 'string') || !(value instanceof String) || !value) 
-    {
-        throw new TypeError(value + ' is not a string or is an empty string!')
-    }
-}
+module.exports = class TypeChecker {
 
-export function isString(value) 
-{
-    if ((typeof value !== 'string') || !(value instanceof String)) 
-    {
-        throw new TypeError(value + ' is not a string!')
-    }
-}
 
-export function isInt(value) 
-{
-    if ((typeof value !== 'number') || value % 1 !== 0 || !(isFinite(value))) 
+    static isValidString(value) 
     {
-        throw new TypeError(value + ' is not an integer!')
+        if ((typeof value !== 'string') || !(value instanceof String) || !value) 
+        {   
+            throw new TypeError(value + ' is not a string or is an empty string!')
+        }
     }
-}
 
-export function isFloat(value) 
-{
-    if ((typeof value !== 'number') || value % 1 === 0 || !(isFinite(value))) 
+    static isString(value) 
     {
-        throw new TypeError(value + ' is not a float!')
+        if ((typeof value !== 'string') || !(value instanceof String)) 
+        {
+            throw new TypeError(value + ' is not a string!')
+        }
     }
-}
 
-export function isBoolean(value) 
-{
-    if (typeof value !== 'boolean') 
+    static isInt(value) 
     {
-        throw new TypeError(value + ' is not a boolean!')
+        if ((typeof value !== 'number') || value % 1 !== 0 || !(isFinite(value))) 
+        {
+            throw new TypeError(value + ' is not an integer!')
+        }
     }
-}
 
-export function isDate(value) 
-{
-    if (!(value instanceof Date)) 
+    static isFloat(value) 
     {
-        throw new TypeError(value + ' is not a Date!')
+        if ((typeof value !== 'number') || value % 1 === 0 || !(isFinite(value))) 
+        {
+            throw new TypeError(value + ' is not a float!')
+        }
     }
-}
 
-export function isInstanceOf(object, objectClass)
-{
-    if (!(object instanceof objectClass))
+    static isBoolean(value) 
     {
-        throw new TypeError(object + ' is not an instance of ' + objectClass + ' class!')
+        if (typeof value !== 'boolean') 
+        {
+            throw new TypeError(value + ' is not a boolean!')
+        }
     }
-}
 
-export function isFunction(value) 
-{
-    if(typeof value !== 'function') {
-        throw new TypeError(value + ' is not a function!')
-    }
-}
-
-export function isEnumOf(object, Enum)
-{
-    if (!Object.values(Enum).includes(object)) 
+    static isDate(value) 
     {
-        throw new TypeError(object + ' is not an enum of ' + Enum + '!')
+        if (!(value instanceof Date)) 
+        {
+            throw new TypeError(value + ' is not a Date!')
+        }
+    }
+
+    static isInstanceOf(object, objectClass)
+    {
+        if (!(object instanceof objectClass))
+        {
+            throw new TypeError(object + ' is not an instance of ' + objectClass + ' class!')
+        }
+    }
+
+    static isFunction(value) 
+    {
+        if(typeof value !== 'function') {
+            throw new TypeError(value + ' is not a function!')
+        }
+    }
+
+    static isEnumOf(object, Enum)
+    {
+        if (!Object.values(Enum).includes(object)) 
+        {
+            throw new TypeError(object + ' is not an enum of ' + Enum + '!')
+        }
     }
 }
