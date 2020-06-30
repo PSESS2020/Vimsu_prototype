969b66022485fe302f8928631a2882df1a545717
LauraTraub <laura-traub@hotmail.de>
2020-06-30 20:02:29 +0200

    added singleton pattern to ClientController.js

--- game/app/client/controller/ClientController.js
+++ game/app/client/controller/ClientController.js
@@ -1,11 +1,20 @@
 class ClientController {
+
+    #gameView;
+
+    //creates an instance of ClientController only if there is not an instance already
     constructor(gameView) {
-        this.gameView = gameView;
+        if (!!ClientController.instance) {
+            return ClientController.instance;
+        }
+
+        ClientController.instance = this;
+
+        this.#gameView = gameView;
+
+        return this;
     }
 
-    getInstance() {
-
-    }
 
     getPortListen() {
         return this.portListen;
