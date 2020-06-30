a9fe6e4132eff14c836fcb72e8ced6c8e6ae1f50
pnschumacher <philipp.schumacher@gmx.de>
2020-06-30 17:07:51 +0200

    Update GameObject.js

--- game/app/server/models/GameObject.js
+++ game/app/server/models/GameObject.js
@@ -1,1 +1,77 @@
+var TypeChecker = require('../../utils/TypeChecker.js');
+var Position = require('./Position.js');
 
+module.exports = class GameObject {
+
+    #id;
+    #name;
+    //creationDate;
+    #width;
+    #length;
+    //height;
+    #position;
+    #isSolid;
+    //isStatic;
+
+
+    /**
+     * Erstellt GameObject-Instanz
+     * 
+     * @author Philipp
+     * 
+     * @param {int} id 
+     * @param {String} name 
+     * @param {int} width 
+     * @param {int} length 
+     * @param {Position} position 
+     * @param {boolean} isSolid 
+     */
+    constructor(id, name, width, length, position, isSolid) {
+        
+        TypeChecker.isInt(id);
+        TypeChecker.isString(name);
+        TypeChecker.isInt(width);
+        TypeChecker.isInt(length);
+        TypeChecker.isInstanceOf(position, Position);
+        TypeChecker.isBoolean(isSolid);
+        
+        this.#id = id;
+        this.#name = name;
+        this.#width = width;
+        this.#length = length;
+        this.#position = position;
+        this.#isSolid = isSolid;
+    }
+
+    getId() {
+        return this.#id;
+    }
+
+    getName() {
+        return this.#name;
+    }
+
+    getWidth() {
+        return this.#width;
+    }
+
+    getLength() {
+        return this.#length;
+    }
+
+    getPosition() {
+        return this.#position;
+    }
+
+    getSolid() {
+        return this.#isSolid;
+    }
+
+    /*
+    getDistance(otherPosition) {
+        TypeChecker.isInstanceOf(otherPosition, Position);
+
+
+    }
+    */
+}
