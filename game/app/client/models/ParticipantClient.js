21e3c78e7744a5b192153c7d7b22cd6b9ba60bab
klaudialeo <klaudialeo@yahoo.com>
2020-06-30 12:30:55 +0200

    Add Participant Model

--- game/app/client/models/ParticipantClient.js
+++ game/app/client/models/ParticipantClient.js
@@ -1,1 +1,37 @@
+import PositionClient from './PositionClient.js'
+import DirectionClient from './DirectionClient.js'
+import * as TypeChecker from '../../utils/TypeChecker.js'
 
+export default class ParticipantClient {
+    
+    constructor(position, direction) 
+    {
+        TypeChecker.isInstanceOf(position, PositionClient);
+        TypeChecker.isEnumOf(direction, DirectionClient);
+
+        this.position = position;
+        this.direction = direction;
+    }
+
+    getPosition() 
+    {
+        return this.position;
+    }
+
+    setPosition(position) 
+    {
+        TypeChecker.isInstanceOf(position, PositionClient);
+        this.position = position;
+    }
+
+    getDirection() 
+    {
+        return this.direction;
+    }
+
+    setDirection(direction) 
+    {
+        TypeChecker.isEnumOf(direction, DirectionClient);
+        this.direction = direction;
+    }
+}
--- game/app/server/models/Participant.js
+++ game/app/server/models/Participant.js
@@ -1,1 +1,64 @@
+import Position from './Position.js'
+import Direction from './Direction.js'
+import ParticipantController from '../../server/controller/ParticipantController.js'
+import * as TypeChecker from '../../utils/TypeChecker.js'
+
+export class Participant {
+
+    constructor(id, position, direction, participantController)
+    {
+        TypeChecker.isInt(id);
+        TypeChecker.isInstanceOf(participantController, ParticipantController);
+
+        this.id = id;
+        this.participantController = participantController;
+
+        if (!position || !direction)
+        {
+            this.position = new Position(1, 1, 1);
+            this.direction = Direction.DOWNRIGHT;
+        }
+
+        else 
+        {
+            TypeChecker.isInstanceOf(position, Position);
+            TypeChecker.isEnumOf(direction, Direction);
+
+            this.position = position;
+            this.direction = direction;
+        }
+    }
+
+    getId() 
+    {
+        return this.id;
+    }
+
+    getPosition() 
+    {
+        return this.position;
+    }
+
+    setPosition(position) 
+    {
+        TypeChecker.isInstanceOf(position, Position);
+        this.position = position;
+    }
+
+    getDirection() {
+        return this.direction;
+    }
+
+    setDirection(direction) 
+    {
+        TypeChecker.isEnumOf(direction, Direction);
+        this.direction = direction;
+    }
+
+    getParticipantController() 
+    {
+        return this.participantController;
+    }
+}
+
 
