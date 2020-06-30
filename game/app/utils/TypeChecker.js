95b3493fa241ff5abba708f69b47298ad1d4f1bb
klaudialeo <klaudialeo@yahoo.com>
2020-06-30 10:55:52 +0200

    Add TypeChecker.js

--- game/app/utils/TypeChecker.js
+++ game/app/utils/TypeChecker.js
@@ -1,1 +1,34 @@
+export function isString(value) 
+{
+    return typeof value === 'string' || value instanceof String;
+}
 
+export function isInt(value) 
+{
+    return typeof value === 'number' && isFinite(value) && value % 1 === 0;
+}
+
+export function isFloat(value) 
+{
+    return typeof value === 'number' && isFinite(value) && value % 1 !== 0;
+}
+
+export function isBoolean(value) 
+{
+    return typeof value === 'boolean';
+}
+
+export function isDate(value) 
+{
+    return value instanceof Date;
+}
+
+export function isInstanceOf(object, objectClass)
+{
+    return object instanceof objectClass;
+}
+
+export function isFunction (value) 
+{
+    return typeof value === 'function';
+}
