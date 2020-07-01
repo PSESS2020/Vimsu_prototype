55d620d95710fd5a34166069eafc24b0ae1e151b
klaudialeo <klaudialeo@yahoo.com>
2020-07-01 21:11:59 +0200

    fix index.js

--- game/app/client/index.js
+++ game/app/client/index.js
@@ -1,18 +1,20 @@
 var GameView = require('./views/js/GameView.js')
-var ClientController = require('./controller/ClientController.js')
+//var ClientController = require('./controller/ClientController.js')
 
-//document.getElementById("demo").innerHTML = "abc";
+document.getElementById("demo").innerHTML = "abc";
 
-//let canvas = document.getElementById("canvas");
-//let ctx = canvas.getContext("2d");
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
 
 const GAME_WIDTH = 600;
 const GAME_HEIGHT = 300;
 
+gameView = new GameView(GAME_WIDTH, GAME_HEIGHT);
+/*
 let gameView = new GameView(GAME_WIDTH, GAME_HEIGHT);
 let clientController = new ClientController(gameView, 1);
 clientController.setPort(3000);
-clientController.openSocketConnection();
+clientController.openSocketConnection();*/
 /*
 //TODO: anpassen
 let lastTime = 0;
