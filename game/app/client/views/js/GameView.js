1845277a77e6af204605af137669873c7fd25159
klaudialeo <klaudialeo@yahoo.com>
2020-07-01 10:29:39 +0200

    Modify a parameter

--- game/app/client/views/js/GameView.js
+++ game/app/client/views/js/GameView.js
@@ -70,7 +70,7 @@ module.exports = class GameView {
 
     /**
      * 
-     * @param {ParticipantClient} participants list of another participants excluding the current client
+     * @param {ParticipantClient} participants array of another participants / an participant instance excluding the current client
      */
     initAnotherAvatarViews(participants)
     {
@@ -79,50 +79,85 @@ module.exports = class GameView {
             throw new Error("Please initialize the current client's avatar view first using initOwnAvatarView(participant)");
         }
 
-        var i;
-        for(i = 0; i < participants.length; i++)
+        if(participants instanceof Array) 
         {
-            TypeChecker.isInstanceOf(participants[i], ParticipantClient);
-
-            if(this.#anotherParticipantAvatarViews.includes(participants[i])) {
-                throw new Error(participants[i] + " is already in list of participants")
-            }
-
-            if(participants[i] !== this.#ownAvatarView) 
+            var i;
+            for(i = 0; i < participants.length; i++)
             {
-                this.#anotherParticipantAvatarViews.push(new AvatarView(participant.getId(), participant.getPosition(), participant.getDirection()));
+                TypeChecker.isInstanceOf(participants[i], ParticipantClient);
+
+                if(this.#anotherParticipantAvatarViews.includes(participants[i])) 
+                {
+                    throw new Error(participants[i] + " is already in list of participants")
+                }
+
+                if(participants[i] !== this.#ownAvatarView) 
+                {
+                    this.#anotherParticipantAvatarViews.push(new AvatarView(participant.getId(), participant.getPosition(), participant.getDirection()));
+                }
             }
         }
+        else 
+        {
+            TypeChecker.isInstanceOf(participants, ParticipantClient);
 
+            if(this.#anotherParticipantAvatarViews.includes(participants)) 
+            {
+                throw new Error(participants + " is already in list of participants")
+            }
+
+            if(participants !== this.#ownAvatarView) 
+            {
+                    this.#anotherParticipantAvatarViews.push(new AvatarView(participant.getId(), participant.getPosition(), participant.getDirection()));
+            }
+        }
 
         /*participants.forEach(participant => 
             TypeChecker.isInstanceOf(participant, ParticipantClient),
             this.#anotherParticipantAvatarViews.push(new AvatarView(participant.getId(), participant.getPosition(), participant.getDirection()))
         );*/
     }
-
+        
     updateAnotherAvatarViews(participants)
     {
 
     }
 
+    /**
+     * 
+     * @param {ParticipantClient} participants array of another participants / an participant instance excluding the current client
+     */
     removeAnotherAvatarViews(participantIds)
     {
-        var i;
-        for (i = 0; i < participantIds.length; i++)
-        {
-            TypeChecker.isInt(id);
 
-            let index = this.#updateList.findIndex(participant => participant.getId() === participantIds[i]);
+        if (participantIds instanceof Array) {
+            var i;
+            for (i = 0; i < participantIds.length; i++)
+            {
+                TypeChecker.isInt(participantIds[i]);
+
+                let index = this.#updateList.findIndex(participant => participant.getId() === participantIds[i]);
+
+                if (index < 0) 
+                {
+                    throw new Error(participantsIds[i] + " is not in list of participants")
+                }
+
+                this.#updateList.splice(index, 1)
+            }
+        }
+        else {
+            TypeChecker.isInt(participantIds);
+            let index = this.#updateList.findIndex(participant => participant.getId() === participantIds);
 
             if (index < 0) 
-            {
-                throw new Error(participantsIds[i] + " is not in list of participants")
-            }
-
+                {
+                    throw new Error(participantsIds + " is not in list of participants")
+                }
+            
             this.#updateList.splice(index, 1)
         }
-
+        
         /*participantIds.forEach(id => 
             TypeChecker.isInt(id),
             this.#updateList.splice(index, 1)
