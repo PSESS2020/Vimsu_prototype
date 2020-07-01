bf5b43d2fae22d02823f0828941c9db9562ef196
klaudialeo <klaudialeo@yahoo.com>
2020-07-01 12:54:28 +0200

    add extension to Views abstract class, function of abstract classes, typechecker

--- game/app/client/views/js/NPCAvatarView.js
+++ game/app/client/views/js/NPCAvatarView.js
@@ -1,11 +1,13 @@
 const AvatarView = require("./AvatarView");
+var TypeChecker = require('../../../utils/TypeChecker.js')
 
-class NPCAvatarView extends AvatarView {
+module.exports = class NPCAvatarView extends AvatarView {
    
     #npcId;
 
     constructor(position, direction, npcId) {
         super(position, direction);
+        TypeChecker.isInt(npcId);
         this.#npcId = npcId;
     }
 
--- game/app/client/views/js/ParticipantAvatarView.js
+++ game/app/client/views/js/ParticipantAvatarView.js
@@ -1,11 +1,13 @@
 const AvatarView = require("./AvatarView");
+var TypeChecker = require('../../../utils/TypeChecker.js')
 
-class ParticipantAvatarView extends AvatarView {
+module.exports = class ParticipantAvatarView extends AvatarView {
 
     #participantId;
 
     constructor(position, direction, participantId) {
         super(position, direction);
+        TypeChecker.isInt(participantId)
         this.#participantId = participantId;
     }
 
