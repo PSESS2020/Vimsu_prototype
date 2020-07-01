35036397de6b28b08aecd6b916b5d63be4429b6e
pnschumacher <philipp.schumacher@gmx.de>
2020-07-01 17:14:03 +0200

    Update ParticipantController.js
    2 new methods

--- game/app/server/controller/ParticipantController.js
+++ game/app/server/controller/ParticipantController.js
@@ -11,4 +11,14 @@ module.exports = class ParticipantController {
     getParticipant() {
         return this.#participant;
     }
+
+    //TODO: Sagt ClientController, dass Teilnehmer mit participantId seine Position geändert hat
+    sendMovementOther(participantId, position) {
+
+    }
+
+    //TODO: Sagt ClientController, dass Teilnehmer mit participantId den Raum verlassen hat
+    sendRoomLeftByOther(participantId) {
+
+    }
 }
