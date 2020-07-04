c74e562c8b0e7f7490ec546022c79a9d4cd107d6
Dopaman <niklas.schmidt@hotmail.de>
2020-07-04 20:14:40 +0200

    Added Walls

--- game/app/client/views/js/FoyerView.js
+++ game/app/client/views/js/FoyerView.js
@@ -2,18 +2,35 @@
 var TypeChecker = require('../../../utils/TypeChecker.js')
 
 module.exports = */class FoyerView extends MapView {
-    #tileColumnOffset = 50;
-    #tileRowOffset = 25;
+    #tileColumnOffset = 64;
+    #tileRowOffset = 32;
+    #wallColumnOffset = 128;
+    #wallRowOffset = 158;
     #originX = 0;
     #originY = 0;
 
 
     constructor() {
         super();
-        this.tileImage = new Image();
-        this.run();
-        this.tileImage.src = FoyerMap.tiles[0];
+        this.load();
     }
+    load() {
+        this.tileImages = new Array();
+        var loadedImages = 0;
+        var totalImages = FoyerMap.tiles.length;
+    
+        // Load all the images before we run the app
+        var self = this;
+        for(var i = 0; i < FoyerMap.tiles.length; i++) {
+          this.tileImages[i] = new Image();
+          this.tileImages[i].onload = () => {
+            if(++loadedImages >= totalImages) {
+              this.run();
+            }
+          };
+          this.tileImages[i].src = FoyerMap.tiles[i];
+        }
+      }
 
     run() {
         this.canvas = $('#canvas');
@@ -40,10 +57,9 @@ module.exports = */class FoyerView extends MapView {
             //mapTilePosX = Math.round(e.pageX / this.#tileColumnOffset - e.pageY / this.#tileRowOffset);
             //mapTilePosY = Math.round(e.pageX / this.#tileColumnOffset + e.pageY / this.#tileRowOffset);
         });
+
         this.updateCanvasSize();
-        this.tileImage.onload = () => {
-            this.draw();
-        };
+        this.draw();
     }
 
     updateCanvasSize() {
@@ -62,17 +78,28 @@ module.exports = */class FoyerView extends MapView {
         
         for(var x = (this.xNumTiles - 1); x >= 0; x--) {
             for(var y = 0; y < this.yNumTiles; y++) {
-                this.drawTile(x, y);
+                
+                var imageType = FoyerMap.map[x][y];
+
+                this.drawTile(imageType, x, y);
             }
         }
     }
 
-    drawTile(x, y) {
+    drawTile(imageType, x, y) {
 
         //calculates the screen position where to draw the tile
         var screenX = x * this.#tileColumnOffset / 2 + y * this.#tileColumnOffset / 2 + this.#originX;
+        if(imageType == 0)
         var screenY = y * this.#tileRowOffset / 2 - x * this.#tileRowOffset / 2 + this.#originY;
-            ctx.drawImage(this.tileImage, screenX, screenY);
+        else if(imageType == 1)
+        var screenY = y * this.#tileRowOffset / 2 - x * this.#tileRowOffset / 2 + this.#originY - this.#wallColumnOffset + this.#tileRowOffset / 2 + 1;
+        else {
+        var screenX = x * this.#tileColumnOffset / 2 + y * this.#tileColumnOffset / 2 + this.#originX - this.#tileColumnOffset;
+        var screenY = y * this.#tileRowOffset / 2 - x * this.#tileRowOffset / 2 + this.#originY - this.#wallColumnOffset + this.#tileRowOffset / 2 + 1;
+        }
+
+        ctx.drawImage(this.tileImages[imageType], screenX, screenY);
     }
 
     drawGameObjects(objectType) {
