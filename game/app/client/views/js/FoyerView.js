5978697ee856e36f53583a5781294c9aa190c475
Dopaman <niklas.schmidt@hotmail.de>
2020-07-04 23:50:23 +0200

    Added doors and doortiles and hopefully fixed map position.

--- game/app/client/views/js/FoyerView.js
+++ game/app/client/views/js/FoyerView.js
@@ -18,24 +18,25 @@ module.exports = */class FoyerView extends MapView {
         this.tileImages = new Array();
         var loadedImages = 0;
         var totalImages = FoyerMap.tiles.length;
-    
+
         // Load all the images before we run the app
         var self = this;
-        for(var i = 0; i < FoyerMap.tiles.length; i++) {
-          this.tileImages[i] = new Image();
-          this.tileImages[i].onload = () => {
-            if(++loadedImages >= totalImages) {
-              this.run();
-            }
-          };
-          this.tileImages[i].src = FoyerMap.tiles[i];
+        for (var i = 0; i < FoyerMap.tiles.length; i++) {
+            this.tileImages[i] = new Image();
+            this.tileImages[i].onload = () => {
+                if (++loadedImages >= totalImages) {
+                    this.run();
+                }
+            };
+            this.tileImages[i].src = FoyerMap.tiles[i];
         }
-      }
+    }
 
     run() {
         this.canvas = $('#canvas');
         this.ctx = this.canvas[0].getContext("2d");
-
+        this.ctx.canvas.width = 1800;
+        this.ctx.canvas.height = 900;
         this.xNumTiles = FoyerMap.map.length;
         this.yNumTiles = FoyerMap.map[0].length;
 
@@ -44,14 +45,14 @@ module.exports = */class FoyerView extends MapView {
             this.draw();
         });
 
-        $(window).on('mousemove', function(e) {
+        $(window).on('mousemove', function (e) {
             //Calc the mouseposition in the game world.
             //Transforms the x,y coordinates of the current mouseposition
             //to the coordinates in the game map.
             //coordinates origin is the middle of the left tile.
             //e.pageX = e.pageX - this.#tileColumnOffset / 2 - this.#originX;
             //e.pageY = e.pageY - this.#tileRowOffset / 2 - this.#originY;
-            
+
             //calculate the coordinates of the tiles.
             //tile origin is the first left tile, it has the transformed position (0,0)
             //mapTilePosX = Math.round(e.pageX / this.#tileColumnOffset - e.pageY / this.#tileRowOffset);
@@ -63,23 +64,26 @@ module.exports = */class FoyerView extends MapView {
     }
 
     updateCanvasSize() {
-        
+
         var width = window.screen.width;
         var height = window.screen.height;
-        this.ctx.canvas.width  = width;
-        this.ctx.canvas.height = height;
+        //this.ctx.canvas.width  = width;
+        //this.ctx.canvas.height = height;
 
-        this.#originX = this.#tileColumnOffset - 45;
-        this.#originY = height / 2 - 20;
-      }
+        this.#originX = this.ctx.canvas.width / 2 - this.xNumTiles * this.#tileColumnOffset / 2;
+        this.#originY = (this.yNumTiles + 3) * this.#tileRowOffset / 2;
+    }
 
 
     draw() {
-        
-        for(var x = (this.xNumTiles - 1); x >= 0; x--) {
-            for(var y = 0; y < this.yNumTiles; y++) {
-                
+
+        for (var x = (this.xNumTiles - 1); x >= 0; x--) {
+            for (var y = 0; y < this.yNumTiles; y++) {
+
                 var imageType = FoyerMap.map[x][y];
+
+                if (imageType == 0)
+                    continue;
 
                 this.drawTile(imageType, x, y);
             }
@@ -90,16 +94,50 @@ module.exports = */class FoyerView extends MapView {
 
         //calculates the screen position where to draw the tile
         var screenX = x * this.#tileColumnOffset / 2 + y * this.#tileColumnOffset / 2 + this.#originX;
-        if(imageType == 0)
         var screenY = y * this.#tileRowOffset / 2 - x * this.#tileRowOffset / 2 + this.#originY;
-        else if(imageType == 1)
-        var screenY = y * this.#tileRowOffset / 2 - x * this.#tileRowOffset / 2 + this.#originY - this.#wallColumnOffset + this.#tileRowOffset / 2 + 1;
-        else {
-        var screenX = x * this.#tileColumnOffset / 2 + y * this.#tileColumnOffset / 2 + this.#originX - this.#tileColumnOffset;
-        var screenY = y * this.#tileRowOffset / 2 - x * this.#tileRowOffset / 2 + this.#originY - this.#wallColumnOffset + this.#tileRowOffset / 2 + 1;
+
+        //because the door image has a diffrent size.
+        var doorOffsetY = this.#tileRowOffset / 2 - this.#wallColumnOffset + 1;
+
+        if (imageType == 2)
+            screenY += doorOffsetY;
+        else if (imageType == 3) {
+            screenX -= this.#tileColumnOffset;
+            screenY += doorOffsetY;
+        } else if (imageType == 4)
+            screenY += doorOffsetY;
+        else if (imageType == 5) {
+            screenX -= this.#tileColumnOffset;
+            screenY += doorOffsetY;
+        }
+        else if (imageType == 6) {
+            this.drawLeftDoorTile(imageType, x, y);
+            return;
+        } else if (imageType == 7) {
+            this.drawRightDoorTile(imageType, x, y);
+            return;
         }
 
-        ctx.drawImage(this.tileImages[imageType], screenX, screenY);
+        ctx.drawImage(this.tileImages[imageType - 1], screenX, screenY);
+    }
+
+    drawLeftDoorTile(imageType, x, y) {
+        if (imageType == 6) {
+
+            var screenX = x * this.#tileColumnOffset / 2 + (y + 1) * this.#tileColumnOffset / 2 + this.#originX;
+            var screenY = (y + 1) * this.#tileRowOffset / 2 - x * this.#tileRowOffset / 2 + this.#originY;
+
+            ctx.drawImage(this.tileImages[0], screenX, screenY);
+        } else throw new Error("Wrong Image Type for the floor of a left door.");
+    }
+    drawRightDoorTile(imageType, x, y) {
+        if (imageType == 7) {
+
+            var screenX = (x - 1) * this.#tileColumnOffset / 2 + y * this.#tileColumnOffset / 2 + this.#originX;
+            var screenY = y * this.#tileRowOffset / 2 - (x - 1) * this.#tileRowOffset / 2 + this.#originY;
+
+            ctx.drawImage(this.tileImages[0], screenX, screenY);
+        } else throw new Error("Wrong Image Type for the floor of a left door.");
     }
 
     drawGameObjects(objectType) {
