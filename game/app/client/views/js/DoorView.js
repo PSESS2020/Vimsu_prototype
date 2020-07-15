4f794ee29950257aa4aa754d82e6c1f9cb086e04
pnschumacher <philipp.schumacher@gmx.de>
2020-07-15 20:17:45 +0200

    Added click handle for Foyer Doors

--- game/app/client/views/js/DoorView.js
+++ game/app/client/views/js/DoorView.js
@@ -30,6 +30,8 @@ class DoorView extends GameObjectView {
         else if (this.#DOORTYPE === GameObjectTypeClient.RECEPTIONDOOR) {
             eventManager.handleReceptionDoorClick();
         }
-        //TODO: Foyer Door Click
+        else if(this.#DOORTYPE === GameObjectTypeClient.FOYERDOOR) {
+            eventManager.handleFoyerDoorClick();
+        }
     }
 }
