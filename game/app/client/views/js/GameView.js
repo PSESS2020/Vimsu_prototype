1acfa653f5b073a497510489927554989eb23c87
klaudialeo <klaudialeo@yahoo.com>
2020-07-18 00:13:50 +0200

    move code from gameview to currentLecturesView and LectureView

--- game/app/client/views/js/GameView.js
+++ game/app/client/views/js/GameView.js
@@ -17,6 +17,8 @@ const ParticipantClient = require('../../models/ParticipantClient.js')*/
     #foodCourtView;
     #receptionView;
     #scheduleListView;
+    #currentLecturesView;
+    #lectureView;
     #currentMap;
     #ownAvatarView;
     #anotherParticipantAvatarViews = [];
@@ -360,21 +362,7 @@ const ParticipantClient = require('../../models/ParticipantClient.js')*/
     }
 
     updateCurrentLectures(lectures) {
-        $('#currentLecturesContainer').empty();
-        
-        lectures.forEach(lecture => {
-            // this is really messy i know, should move it somewhere else
-            $('#currentLecturesContainer').append(`
-                <div class="currentLecturesContainer d-flex flex-column align-items-start col-4 m-1 pt-2">
-                    <h5>${lecture.title}</h5>
-                    <div class="small">${lecture.oratorName + " || " + lecture.maxParticipants + " seats"}</div>
-                    <div>${lecture.remarks}</div>
-                    <button id=('${lecture.id}') class="btn btn-lecture m-2 align-self-end mt-auto" onclick="(new EventManager()).handleLectureClicked('${lecture.id}')">Show</button>
-                </div>
-            `)
-        });
-
-        $('#currentLectures').show(); // TODO: maybe move somewhere else if logic requires it
+        this.#currentLecturesView = new CurrentLecturesView().draw(lectures);
     }
 
     initCurrentSchedule(lectures) {
@@ -382,14 +370,7 @@ const ParticipantClient = require('../../models/ParticipantClient.js')*/
     }
 
     updateCurrentLecture(lecture) {
-        $('#currentLectures').hide(); // hide the overview of current lectures
-
-        $('#lectureTitleLabel').text(lecture.title);
-        $('#lectureSpeakerLabel').text(lecture.oratorName);
-        $('#lectureVideo').attr('src', lecture.videoUrl);
-        $('#lectureVideo').load();
-        
-        $('#lectureVideoWindow').show();
+        this.#lectureView = new LectureView().draw(lecture);
     }
         
     updateOwnAvatarRoom(typeOfRoom) {
---
+++ game/app/client/views/js/LectureView.js
@@ -0,0 +1,18 @@
+class LectureView extends WindowView {
+
+    constructor(){
+        super();
+    }
+
+    draw(lecture) {
+        $('#currentLectures').hide(); // hide the overview of current lectures
+
+        $('#lectureTitleLabel').text(lecture.title);
+        $('#lectureSpeakerLabel').text(lecture.oratorName);
+        $('#lectureVideo').attr('src', lecture.videoUrl);
+        $('#lectureVideo').load();
+        
+        $('#lectureVideoWindow').show();
+    }
+
+}
