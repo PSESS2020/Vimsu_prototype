5aece53beaec328d2e7db5df60ed63f0b51f3ba0
klaudialeo <klaudialeo@yahoo.com>
2020-07-25 01:35:37 +0200

    fix typo

--- website/controller/RouteController.js
+++ website/controller/RouteController.js
@@ -333,7 +333,7 @@ module.exports = class RouteController {
                 request.session.email = res.getEmail();
                 response.redirect('/account');
             }).catch(err => {
-                response.send('Failed updating data. <a href="/account">Try again</a>');
+                response.send('Failed updating data. <a href="/editAccount">Try again</a>');
                 console.error(err);
             })
         })
