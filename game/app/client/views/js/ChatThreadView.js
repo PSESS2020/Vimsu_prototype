a1a03263b3842583af36a3dab92b4a09eca0c706
Dopaman <niklas.schmidt@hotmail.de>
2020-07-30 17:05:40 +0200

    hotfix

--- game/app/client/views/js/ChatThreadView.js
+++ game/app/client/views/js/ChatThreadView.js
@@ -37,44 +37,16 @@ class ChatThreadView extends WindowView {
     
     draw(chat) {
         /* Get all the messages and draw them */
-<<<<<<< HEAD
-        /*
-        this.#chatId = chat.chatId;
-=======
         this.#chat = chat;
->>>>>>> 6e8c17cde42df5369e45ee5fbef633ebdf5417e4
         this.#messages = chat.messages;
         $('#chatThreadModalTitle').empty();
         $('#chatThreadModalTitle').text(chat.title);
 
         $('#chatThreadModal .modal-body .list-group').empty()
         
-<<<<<<< HEAD
-        //draw the messages
-        this.#update(this.#messages);
-        
-        var script = `
-            <script>
-                $('#chatInput').submit( function(event) {
-                    event.preventDefault();
-                    let messageVal = $('#chatMessageInput').val();
-    
-                    if(messageVal !== '') {
-                        
-                        new EventManager.handleChatMessageInput("${chat.chatId}", messageVal);
-      
-                    $('#chatMessageInput').val('');
-                    return false;
-                    }
-                });
-            </script>
-        `;
-        */
-=======
         this.#messages.forEach((message) => {
             this.#appendMessage(message);
         })
->>>>>>> 6e8c17cde42df5369e45ee5fbef633ebdf5417e4
     };
     
     addNewMessage(chatId, message) {
@@ -86,44 +58,6 @@ class ChatThreadView extends WindowView {
         this.#appendMessage(message);
     };
     
-<<<<<<< HEAD
-    getChatId() {
-        return this.#chatId;
-    }
-
-    #update = function(messages) {
-        if(messages) {
-            messages.forEach( (message) => {
-                /* Clear view to make sure we don't draw anything twice */
-                $('#chatThreadModal .modal-body .list-group').empty()
-                
-                // TODO
-                // Draw the message slightly different if the client did send them
-                // Different color, profile picture on the right?
-                
-                var messageDiv = `
-                <ul id="${"chat" + chat.chatId}">
-                    <li class="list-group-item bg-transparent" >
-                        <div class="row w-100">
-                            <div class="col-12 col-sm-2 px-0">
-                                <i class="fa fa-user fa-5x navbarIcons" style="margin-left: 5px" ></i>
-                            </div>
-                            <div class="col-12 col-md-9 text-center text-sm-left">
-                                <label class="name lead">${chat.title}</label>
-                                <br> 
-                                <span >${"[" + message.timestamp + "]" + message.username + ": "}</span>
-                                <br>
-                                <span class="small">${message.text}</span>
-                            </div>  
-                        </div>
-                    </li>
-                </ul>
-                `;
-                
-                $('#chatThreadModal .modal-body .list-group').prepend(messageDiv);
-            });
-        }
-=======
     #appendMessage = (message) => {
         var messageDiv = `
         <div>
@@ -133,7 +67,6 @@ class ChatThreadView extends WindowView {
         `;
         
         $('#chatThreadModal .modal-body .list-group').append(messageDiv);
->>>>>>> 6e8c17cde42df5369e45ee5fbef633ebdf5417e4
     }
     
 }
