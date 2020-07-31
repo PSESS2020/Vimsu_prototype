e6e606cea789f1f9f354e16e7f1da1023215bff0
Dopaman <niklas.schmidt@hotmail.de>
2020-07-31 03:51:17 +0200

    prevent avatarmoving on chatinput.

--- game/app/client/index.js
+++ game/app/client/index.js
@@ -87,9 +87,9 @@ document.getElementById("groupName").onkeydown = function(event) {
   event.stopPropagation();
 };
 
-/*document.getElementById("chatInput").onkeydown = function(event) {
+document.getElementById("chatMessageInput").onkeydown = function(event) {
   event.stopPropagation();
-};*/
+};
 
 document.body.onkeydown = function(event) {
   /* This little code-block (plus the one on the bottom) prevents a single input from being
