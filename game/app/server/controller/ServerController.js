1d588a1656a582781b63fe25288db795f78346d8
Dopaman <niklas.schmidt@hotmail.de>
2020-08-02 18:00:58 +0200

    workaround leave bug.

--- game/app/server/controller/ServerController.js
+++ game/app/server/controller/ServerController.js
@@ -1274,15 +1274,16 @@ module.exports = class ServerController {
                 if (remover !== undefined && remover.isMemberOfChat(chatId)){
                     //console.log('from server 2 ' + msgText);
                     //gets list of chat participants for removing participant in their chat.
-                    let chatPartnerIDList = remover.getChat(chatId).getParticipantList();
+                    var chatPartnerIDList = remover.getChat(chatId).getParticipantList();
                     console.log("before " + chatPartnerIDList);
                         for(let i = 0, n = chatPartnerIDList.length; i < n; i++) {
                             
-                            console.log("inside chatPartner " + chatPartnerIDList[i]);
-                            let chatPartner = this.#ppants.get(chatPartnerIDList);
+                            console.log("inside chatPartner id " + chatPartnerIDList[i]);
+                            console.log("inside chatPartner list " + chatPartnerIDList);
+                            let chatPartner = this.#ppants.get(chatPartnerIDList[i]);
                             console.log("inside chatPartner " + chatPartnerIDList[i] + " " + i);
                             //Checks if receiver of message is online
-                            if (chatPartner !== undefined) {
+                            if (chatPartnerIDList[i] !== removerId && chatPartner !== undefined) {
                                
                                 let chatPartnerChat = chatPartner.getChat(chatId);
                                 chatPartnerChat.removeParticipant(removerId);
