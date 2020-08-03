ecdcad85ff73e396c0f661bc7a53f5d912aa8f3b
klaudialeo <klaudialeo@yahoo.com>
2020-08-03 12:50:22 +0200

    still in style fixing mode

--- game/app/client/views/js/ChatThreadView.js
+++ game/app/client/views/js/ChatThreadView.js
@@ -118,7 +118,7 @@ class ChatThreadView extends WindowView {
 
         var messageDiv = `
         <div>
-            <small class="p-1" style="opacity: 0.3; float: right;">${timestamp}</small>
+            <small style="opacity: 0.3; float: right;">${timestamp}</small><br>
             <small><b>${senderUsername}</b></small>
             <small class="wrapword">${message.msgText}</small>
         </div>
