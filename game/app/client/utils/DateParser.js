5217a67609682ca4b6996be71ad202946af02ebc
klaudialeo <klaudialeo@yahoo.com>
2020-08-03 15:14:41 +0200

    show month as text

--- game/app/client/utils/DateParser.js
+++ game/app/client/utils/DateParser.js
@@ -8,9 +8,10 @@ class DateParser {
 
     parse() {
         var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-        var parsedDate = days[this.#date.getDay()] + ", " +(this.#date.getDate()<10?'0':'') + this.#date.getDate() + "/" 
-        + (this.#date.getMonth()<10?'0':'') + (this.#date.getMonth() + 1) + "/" + this.#date.getFullYear() 
+        var parsedDate = days[this.#date.getDay()] + ", " +(this.#date.getDate()<10?'0':'') + this.#date.getDate() + " " 
+        + months[this.#date.getMonth()] + " " + this.#date.getFullYear() 
         + " " + (this.#date.getHours()<10?'0':'') + this.#date.getHours() + 
         ":" + (this.#date.getMinutes()<10?'0':'') + this.#date.getMinutes();
         
