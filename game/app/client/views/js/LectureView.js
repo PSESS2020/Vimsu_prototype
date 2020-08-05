e2e121a612acc14bd1576a309d703b074be14dbd
Eric Ritte <upebw@student.kit.edu>
2020-08-05 23:10:23 +0200

    Pre merge commit

--- game/app/client/views/js/LectureView.js
+++ game/app/client/views/js/LectureView.js
@@ -272,20 +272,9 @@ class LectureView extends WindowView {
     };
 
 }
-<<<<<<< HEAD
-=======
-
-
-const LectureStatus = Object.freeze
-({
-    PENDING: "PENDING",
-    RUNNING: "RUNNING",
-    OVER: "OVER"
-});
 
 const NoTokenMessage = Object.freeze({
     LEFTTOOLONG: "You left the lecture for too long. Therefore, you are not able to ask questions in the lecture chat.",
     REVOKED: "Your token was revoked by either the orator or a moderator. Therefore, you are no longer able to ask questions in the lecture chat. " +
             "Please remember to follow chat etiquette."
 })
->>>>>>> LectureChatCommands
