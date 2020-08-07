923c7b34a5bc55b6399d0e49ede049d23ef9ad29
Eric Ritte <upebw@student.kit.edu>
2020-08-07 11:03:49 +0200

    Added the weekdays.js which i had previously forgotten to do

---
+++ test/server/models/TestData/Weekdays.js
@@ -0,0 +1,9 @@
+module.exports = Object.freeze({
+    MONDAY: "Monday",
+    TUESDAY: "Tuesday",
+    WEDNESDAY: "Wednesday",
+    THURSDAY: "Thursday",
+    FRIDAY: "Friday",
+    SATURDAY: "Saturday",
+    SUNDAY: "Sunday"
+})
