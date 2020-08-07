0abf990ce166358191eebf373a4e2c9c93726d21
klaudialeo <klaudialeo@yahoo.com>
2020-08-07 09:58:29 +0200

    add AccountTest

---
+++ test/server/models/AccountTest.js
@@ -0,0 +1,48 @@
+
+const Account = require('../../../website/models/Account')
+const expect = require('chai').expect;
+
+var accountID = "53f";
+var username = "maxmust";
+var title = "Prof.";
+var surname = "Mustermann";
+var forename = "Max";
+var job = "Professor";
+var company = "KIT";
+var email = "maxmustermann@kit.edu";
+var account = new Account(accountID, username, title, surname, forename, job, company, email);
+
+
+describe("Test getter", function() {
+    it("Test getAccountID", function() {
+        expect(account.getAccountID()).to.be.a('string').and.equal(accountID);
+    })    
+
+    it("Test getUsername", function() {
+        expect(account.getUsername()).to.be.a('string').and.equal(username);
+    })
+
+    it("Test getTitle", function() {
+        expect(account.getTitle()).to.be.a('string').and.equal(title);
+    })
+
+    it("Test getSurname", function() {
+        expect(account.getSurname()).to.be.a('string').and.equal(surname);
+    })
+
+    it("Test getForename", function() {
+        expect(account.getForename()).to.be.a('string').and.equal(forename);
+    })
+
+    it("Test getJob", function() {
+        expect(account.getJob()).to.be.a('string').and.equal(job);
+    })
+
+    it("Test getCompany", function() {
+        expect(account.getCompany()).to.be.a('string').and.equal(company);
+    })
+
+    it("Test getEmail", function() {
+        expect(account.getEmail()).to.be.a('string').and.equal(email);
+    })
+})
