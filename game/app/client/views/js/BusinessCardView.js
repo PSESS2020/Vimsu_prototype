6c7a2096fee3521ee7215d61bfe7ad398c50a46a
klaudialeo <klaudialeo@yahoo.com>
2020-08-09 23:24:05 +0200

    fix bug in businesscardview

--- game/app/client/views/js/BusinessCardView.js
+++ game/app/client/views/js/BusinessCardView.js
@@ -11,9 +11,8 @@ class BusinessCardView extends WindowView {
     }
 
     draw() {
-
-        $('#businessCardModal .modal-body').append(`
-                
+        if(this.#isFriend) {
+            $('#businessCardModal .modal-body').append(`
                 <h5 style="background-color: rgba(24, 30, 32, 0.699); padding: 5px; text-align: center">
                 <i class="fa fa-user-circle pr-2 navbarIcons" style="transform: scale(1)"></i>
                 ${this.#businessCard.getTitle() + " " + this.#businessCard.getForename() + " " + this.#businessCard.getSurname() + " (@" + this.#businessCard.getUsername() + ")"}</h5>
@@ -23,37 +22,48 @@ class BusinessCardView extends WindowView {
                         <td style="border-right: 1pt solid antiquewhite; text-align: right; padding: 15px" >Profession</td>
                         <td style="padding: 15px">${this.#businessCard.getJob() + " at " + this.#businessCard.getCompany()}</td>
                     </tr>
-        `)
-
-        if(this.#isFriend) {
-            $('#businessCardModal .modal-body #profile').append(`
-                <tr>
-                    <td style="border-right: 1pt solid antiquewhite ; text-align: right; padding: 15px">Email</td>
-                    <td style="padding: 15px">${this.#businessCard.getEmail()}</td>
-                </tr>
+                    <tr>
+                        <td style="border-right: 1pt solid antiquewhite ; text-align: right; padding: 15px">Email</td>
+                        <td style="padding: 15px">${this.#businessCard.getEmail()}</td>
+                    </tr>
+                </table>
+                </br>
+                <button id="${"chatnow" + this.#businessCard.getParticipantId()}" class="btn btn-lecture mx-auto d-block">Chat</button>
+                </br>
+                <script> 
+                    $('#chatnow' + '${this.#businessCard.getParticipantId()}').on('click', function (event) {
+                        $('#businessCardModal').modal('hide');
+                        new EventManager().handleChatNowClicked('${this.#businessCard.getParticipantId()}', '${this.#businessCard.getUsername()}');
+                    })
+                </script>
             `)
         } else {
-            $('#businessCardModal .modal-body #profile').append(`
-                <tr>
-                    <td style="border-right: 1pt solid antiquewhite ; text-align: right; padding: 15px">Rank</td>
-                    <td style="padding: 15px">${this.#rank}</td>
-                </tr>
+            $('#businessCardModal .modal-body').append(`
+                <h5 style="background-color: rgba(24, 30, 32, 0.699); padding: 5px; text-align: center">
+                <i class="fa fa-user-circle pr-2 navbarIcons" style="transform: scale(1)"></i>
+                ${this.#businessCard.getTitle() + " " + this.#businessCard.getForename() + " " + this.#businessCard.getSurname() + " (@" + this.#businessCard.getUsername() + ")"}</h5>
+                </br>
+                <table id="profile" style = "color: antiquewhite; width:100%; margin-left: 0">
+                    <tr>
+                        <td style="border-right: 1pt solid antiquewhite; text-align: right; padding: 15px" >Profession</td>
+                        <td style="padding: 15px">${this.#businessCard.getJob() + " at " + this.#businessCard.getCompany()}</td>
+                    </tr>
+                    <tr>
+                        <td style="border-right: 1pt solid antiquewhite ; text-align: right; padding: 15px">Rank</td>
+                        <td style="padding: 15px">${this.#rank}</td>
+                    </tr>
+                </table>
+                </br>
+                <button id="${"chatnow" + this.#businessCard.getParticipantId()}" class="btn btn-lecture mx-auto d-block">Chat</button>
+                </br>
+                <script> 
+                    $('#chatnow' + '${this.#businessCard.getParticipantId()}').on('click', function (event) {
+                        $('#businessCardModal').modal('hide');
+                        new EventManager().handleChatNowClicked('${this.#businessCard.getParticipantId()}', '${this.#businessCard.getUsername()}');
+                    })
+                </script>
             `)
         }
-                    
-        $('#businessCardModal .modal-body').append(`
-            </table>
-            </br>
-            <button id="${"chatnow" + this.#businessCard.getParticipantId()}" class="btn btn-lecture mx-auto d-block">Chat</button>
-            </br>
-            <script> 
-                $('#chatnow' + '${this.#businessCard.getParticipantId()}').on('click', function (event) {
-                    $('#businessCardModal').modal('hide');
-                    new EventManager().handleChatNowClicked('${this.#businessCard.getParticipantId()}', '${this.#businessCard.getUsername()}');
-                })
-            </script>
-
-        `)
     
         $('#businessCardModal').on('hidden.bs.modal', function (e) {
             $('#businessCardModal .modal-body').empty()
