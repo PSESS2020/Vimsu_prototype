eb7eeeda99ef2f80e1c68795b7694dca7c93291e
klaudialeo <klaudialeo@yahoo.com>
2020-08-15 11:59:08 +0200

    fix offset in reception room

--- game/app/server/models/ReceptionRoomDecorator.js
+++ game/app/server/models/ReceptionRoomDecorator.js
@@ -16,12 +16,12 @@ module.exports = class ReceptionRoomDecorator extends RoomDecorator {
         "leftwall_default": "client/assets/wall1.png",
         "rightwall_default": "client/assets/wall2.png",
         "leftfoyerdoor_default": "client/assets/door_foyer.png",
-        "table_default": "client/assets/table.png",
         "leftconferencelogo_default0": "client/assets/conferencelogo1.png",
         "leftconferencelogo_default1": "client/assets/conferencelogo2.png",
         "leftconferencelogo_default2": "client/assets/conferencelogo3.png",
         "leftconferencelogo_default3": "client/assets/conferencelogo4.png",
         "leftconferencelogo_default4": "client/assets/conferencelogo5.png",
+        "table_default": "client/assets/table.png",
     }
 
     constructor(room) {
