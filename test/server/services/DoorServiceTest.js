c27491241565531ec54a912d85eb8328da572069
pnschumacher <philipp.schumacher@gmx.de>
2020-08-16 12:30:22 +0200

    Adjusted DoorServiceTest

--- test/server/services/DoorServiceTest.js
+++ test/server/services/DoorServiceTest.js
@@ -10,67 +10,108 @@ const Direction = require('../../../game/app/client/shared/Direction');
 describe('DoorService test', function () {
     it('test create LectureDoor', function() {
         let doorService = new DoorService();
-        let mapPosition = new Position(TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0));
-        let enterPosition = new Position(TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0));
-        let enterPositions = [enterPosition];
-        let lectureDoor = doorService.createLectureDoor(mapPosition, enterPositions);
+        let roomId = TestUtil.randomIntWithMin(0);
+        let cordX = TestUtil.randomIntWithMin(0);
+        let cordY = TestUtil.randomIntWithMin(0);
+        let mapPosition = new Position(roomId, cordX, cordY);
+        let lectureDoor = doorService.createLectureDoor(mapPosition);
         
+        let expectedEnterPositions = [];
+        let lastEnterPosition;
+        for (var i = mapPosition.getCordX() - 2; i <= mapPosition.getCordX() + 2; i++) {
+            for (var j = mapPosition.getCordY() + 1; j <= mapPosition.getCordY() + 3; j++) {
+                lastEnterPosition = new Position(roomId, i, j);
+                expectedEnterPositions.push(new Position(roomId, i, j));
+            }
+        }
+
         expect(lectureDoor).to.be.instanceOf(Door);
         expect(lectureDoor.getMapPosition()).to.equal(mapPosition);
         expect(lectureDoor.getTargetPosition()).to.equal(undefined);
         expect(lectureDoor.getDirection()).to.equal(undefined);
-        expect(lectureDoor.getEnterPositions()).to.equal(enterPositions);
-        expect(lectureDoor.isValidEnterPosition(enterPosition)).to.be.true;
+        expect(lectureDoor.getEnterPositions()).to.eql(expectedEnterPositions);
+        expect(lectureDoor.isValidEnterPosition(lastEnterPosition)).to.be.true;
     });
 
     it('test create FoyerDoor', function() {
         let doorService = new DoorService();
-        let mapPosition = new Position(TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0));
-        let enterPosition = new Position(TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0));
-        let enterPositions = [enterPosition];
+        let roomId = TestUtil.randomIntWithMin(0);
+        let cordX = TestUtil.randomIntWithMin(0);
+        let cordY = TestUtil.randomIntWithMin(0);
+        let mapPosition = new Position(roomId, cordX, cordY);
         let targetPosition = new Position(TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0));
         let direction = Direction.DOWNRIGHT;
-        let foyerDoor = doorService.createFoyerDoor(mapPosition, enterPositions, targetPosition, direction);
+        let foyerDoor = doorService.createFoyerDoor(mapPosition, targetPosition, direction);
+
+        let expectedEnterPositions = [];
+        let lastEnterPosition;
+        for (var i = mapPosition.getCordX() - 2; i <= mapPosition.getCordX() + 2; i++) {
+            for (var j = mapPosition.getCordY() + 1; j <= mapPosition.getCordY() + 3; j++) {
+                lastEnterPosition = new Position(roomId, i, j);
+                expectedEnterPositions.push(new Position(roomId, i, j));
+            }
+        }
 
         expect(foyerDoor).to.be.instanceOf(Door);
         expect(foyerDoor.getMapPosition()).to.equal(mapPosition);
         expect(foyerDoor.getTargetPosition()).to.equal(targetPosition);
         expect(foyerDoor.getDirection()).to.equal(direction);
-        expect(foyerDoor.getEnterPositions()).to.equal(enterPositions);
-        expect(foyerDoor.isValidEnterPosition(enterPosition)).to.be.true;
+        expect(foyerDoor.getEnterPositions()).to.eql(expectedEnterPositions);
+        expect(foyerDoor.isValidEnterPosition(lastEnterPosition)).to.be.true;
     });
 
     it('test create FoodCourtDoor', function() {
         let doorService = new DoorService();
-        let mapPosition = new Position(TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0));
-        let enterPosition = new Position(TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0));
-        let enterPositions = [enterPosition];
+        let roomId = TestUtil.randomIntWithMin(0);
+        let cordX = TestUtil.randomIntWithMin(0);
+        let cordY = TestUtil.randomIntWithMin(0);
+        let mapPosition = new Position(roomId, cordX, cordY);
         let targetPosition = new Position(TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0));
         let direction = Direction.DOWNRIGHT;
-        let foodCourtDoor = doorService.createFoodCourtDoor(mapPosition, enterPositions, targetPosition, direction);
+        let foodCourtDoor = doorService.createFoodCourtDoor(mapPosition, targetPosition, direction);
+
+        let expectedEnterPositions = [];
+        let lastEnterPosition;
+        for (var i = mapPosition.getCordX() - 3; i <= mapPosition.getCordX() - 1; i++) {
+            for (var j = mapPosition.getCordY() - 2; j <= mapPosition.getCordY() + 2; j++) {
+                lastEnterPosition = new Position(roomId, i, j);
+                expectedEnterPositions.push(new Position(roomId, i, j));
+            }
+        }
 
         expect(foodCourtDoor).to.be.instanceOf(Door);
         expect(foodCourtDoor.getMapPosition()).to.equal(mapPosition);
         expect(foodCourtDoor.getTargetPosition()).to.equal(targetPosition);
         expect(foodCourtDoor.getDirection()).to.equal(direction);
-        expect(foodCourtDoor.getEnterPositions()).to.equal(enterPositions);
-        expect(foodCourtDoor.isValidEnterPosition(enterPosition)).to.be.true;
+        expect(foodCourtDoor.getEnterPositions()).to.eql(expectedEnterPositions);
+        expect(foodCourtDoor.isValidEnterPosition(lastEnterPosition)).to.be.true;
     });
 
     it('test create ReceptionDoor', function() {
         let doorService = new DoorService();
-        let mapPosition = new Position(TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0));
-        let enterPosition = new Position(TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0));
-        let enterPositions = [enterPosition];
+        let roomId = TestUtil.randomIntWithMin(0);
+        let cordX = TestUtil.randomIntWithMin(0);
+        let cordY = TestUtil.randomIntWithMin(0);
+        let mapPosition = new Position(roomId, cordX, cordY);
         let targetPosition = new Position(TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0), TestUtil.randomIntWithMin(0));
         let direction = Direction.DOWNRIGHT;
-        let receptionDoor = doorService.createReceptionDoor(mapPosition, enterPositions, targetPosition, direction);
+        let receptionDoor = doorService.createReceptionDoor(mapPosition, targetPosition, direction);
+
+        let expectedEnterPositions = [];
+        let lastEnterPosition;
+        for (var i = mapPosition.getCordX() - 3; i <= mapPosition.getCordX() - 1; i++) {
+            for (var j = mapPosition.getCordY() - 2; j <= mapPosition.getCordY() + 2; j++) {
+                lastEnterPosition = new Position(roomId, i, j);
+                expectedEnterPositions.push(new Position(roomId, i, j));
+            }
+        }
 
         expect(receptionDoor).to.be.instanceOf(Door);
         expect(receptionDoor.getMapPosition()).to.equal(mapPosition);
         expect(receptionDoor.getTargetPosition()).to.equal(targetPosition);
         expect(receptionDoor.getDirection()).to.equal(direction);
-        expect(receptionDoor.getEnterPositions()).to.equal(enterPositions);
-        expect(receptionDoor.isValidEnterPosition(enterPosition)).to.be.true;
+        expect(receptionDoor.getEnterPositions()).to.eql(expectedEnterPositions);
+        expect(receptionDoor.isValidEnterPosition(lastEnterPosition)).to.be.true;
     });
+
 });
