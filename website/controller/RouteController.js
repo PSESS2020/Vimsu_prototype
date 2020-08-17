13490e9628a45347f42bb6f49e541ff620e1940a
klaudialeo <klaudialeo@yahoo.com>
2020-08-18 00:01:34 +0200

    remove unnecessary method

--- website/controller/RouteController.js
+++ website/controller/RouteController.js
@@ -149,42 +149,6 @@ module.exports = class RouteController {
             }
         })
 
-        this.#app.get('/game/video/:videoName', (request, response) => {
-            if (request.session.loggedin === true) {
-                var videoPath = path.join(__dirname + '../../../config/download/' + request.params.videoName);
-                const stat = fs.statSync(videoPath);
-                const fileSize = stat.size;
-                const range = request.headers.range;
-
-                if (range) {
-                    const parts = range.replace(/bytes=/, "").split("-")
-                    const start = parseInt(parts[0], 10)
-                    const end = parts[1]
-                        ? parseInt(parts[1], 10)
-                        : fileSize - 1
-                    const chunksize = (end - start) + 1
-                    const file = fs.createReadStream(videoPath, { start, end })
-                    const head = {
-                        'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-                        'Accept-Ranges': 'bytes',
-                        'Content-Length': chunksize,
-                        'Content-Type': 'video/mp4',
-                    }
-                    response.writeHead(206, head);
-                    file.pipe(response);
-                } else {
-                    const head = {
-                        'Content-Length': fileSize,
-                        'Content-Type': 'video/mp4',
-                    }
-                    response.writeHead(200, head)
-                    fs.createReadStream(videoPath).pipe(response)
-                }
-            } else {
-                response.redirect('/');
-            }
-        })
-
         this.#app.post('/login', (request, response) => {
             username = request.body.username;
             var password = request.body.password;
