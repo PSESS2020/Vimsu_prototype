b8c97fa649c84c1ac46549c0fae4ed291ba54f10
Dopaman <niklas.schmidt@hotmail.de>
2020-08-18 17:57:33 +0200

    Fixed replaced bad gameloop.

--- game/app/client/index.js
+++ game/app/client/index.js
@@ -1,26 +1,65 @@
-var ctx_map = document.getElementById("mapCanvas").getContext("2d");
-var ctx_avatar = document.getElementById("avatarCanvas").getContext("2d");
-var ctx_ui = document.getElementById("uiCanvas").getContext("2d");
 
 const GAME_WIDTH = GameConfig.CTX_WIDTH;
 const GAME_HEIGHT = GameConfig.CTX_HEIGHT;
+let ctx_map;
+let ctx_avatar;
+let ctx_ui;
 
-this.ctx_map.canvas.width = GAME_WIDTH;
-this.ctx_map.canvas.height = GAME_HEIGHT;
+let clientController;
 
-this.ctx_avatar.canvas.width = GAME_WIDTH;
-this.ctx_avatar.canvas.height = GAME_HEIGHT;
+window.onload = this.init();
 
-this.ctx_ui.canvas.width = GAME_WIDTH;
-this.ctx_ui.canvas.height = GAME_HEIGHT;
+function init() {
+  ctx_map = document.getElementById("mapCanvas").getContext("2d");
+  ctx_avatar = document.getElementById("avatarCanvas").getContext("2d");
+  ctx_ui = document.getElementById("uiCanvas").getContext("2d");
 
-//let gameView = new GameView(GAME_WIDTH, GAME_HEIGHT);
+  ctx_map.canvas.width = GAME_WIDTH;
+  ctx_map.canvas.height = GAME_HEIGHT;
 
-let clientController = new ClientController();
-clientController.setPort(GameConfig.PORT);
-clientController.openSocketConnection();
-//clientController.initGameView();
+  ctx_avatar.canvas.width = GAME_WIDTH;
+  ctx_avatar.canvas.height = GAME_HEIGHT;
 
+  ctx_ui.canvas.width = GAME_WIDTH;
+  ctx_ui.canvas.height = GAME_HEIGHT;
+
+  clientController = new ClientController();
+  clientController.setPort(GameConfig.PORT);
+  clientController.openSocketConnection();
+
+  // Start the first frame request
+  window.requestAnimationFrame(gameLoop);
+}
+
+let secondsPassed;
+let oldTimeStamp;
+let fps;
+
+function gameLoop(timeStamp) {
+  
+    // Calculate the number of seconds passed since the last frame
+    secondsPassed = (timeStamp - oldTimeStamp) / 1000;
+    oldTimeStamp = timeStamp;
+
+    // Calculate fps
+    fps = Math.round(1 / secondsPassed);
+
+    //Clear canvas
+    ctx_avatar.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+    
+    // Perform the drawing operation
+    clientController.updateGame();
+
+    // Draw number to the screen
+    ctx_avatar.fillStyle = 'white';
+    ctx_avatar.fillRect(GAME_WIDTH - 50, 0, 50, 50);
+    ctx_avatar.font = '25px Arial';
+    ctx_avatar.fillStyle = 'black';
+    ctx_avatar.fillText("FPS: " + fps, GAME_WIDTH - 50, 0);
+    // The loop function has reached it's end. Keep requesting new frames
+    window.requestAnimationFrame(gameLoop);
+}
+/*
 setInterval(function () {
   //let deltaTime = timestamp - lastTime;
   //lastTime = timestamp;
@@ -30,7 +69,7 @@ setInterval(function () {
   clientController.updateGame();
   //gameView.update();
   //gameView.draw();  
-}, GameConfig.TIME_DELTA);
+}, GameConfig.TIME_DELTA);*/
 
 $('#allchat').submit(function (event) {
 
