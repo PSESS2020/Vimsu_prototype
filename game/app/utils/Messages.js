673baea2adda081867f6ac980d8982ed6bbb8de0
klaudialeo <klaudialeo@yahoo.com>
2020-08-20 20:18:44 +0200

    fix some typos

--- game/app/utils/Messages.js
+++ game/app/utils/Messages.js
@@ -5,25 +5,25 @@ module.exports = Object.freeze({
                     "\\log --  Will show a log of all messages send into the lecture chat" +
                     ", including the messageID and senderID of each message.",
                     "\\showallby <list of usernames> -- Takes a list of usernames, each one " +
-                    "seperated from the next by a whitespace-character, and displays all messages " +
+                    "separated from the next by a whitespace-character, and displays all messages " +
                     "send into the chat by these users.", 
                     "\\rmuser <list of usernames>  --  Takes a list of usernames, each one " +
-                    "seperated from the next by a whitespace-character, and removes all of them from " +
+                    "separated from the next by a whitespace-character, and removes all of them from " +
                     "the lecture. They will not be able to reenter the lecture.\n WARNING: It is " +
                     "not yet possible to revert this!",
-                    "\\rmmsg <list of msgIDs>  --  Takes a list of messageIDs, each one seperated from the " +
+                    "\\rmmsg <list of msgIDs>  --  Takes a list of messageIDs, each one separated from the " +
                     "next one by a whitespace character, and removes the corresponding messages - " +
                     "if they exist - from the lecture chat. Will also send a warning to " +
                     "the senders of the messages, reminding them to follow chat etiquette.",
                     "\\rmallby <list of usernames>  --  Takes a list of usernames, each one " +
-                    "seperated from the next by a whitespace-character, and removes all messages posted " +
+                    "separated from the next by a whitespace-character, and removes all messages posted " +
                     "by them into the lecture chat. Will also send a warning " +
                     "to these participants, reminding them to follow chat-etiquette.",
                     "\\revoke <list of usernames> --  Takes a list of usernames, each one " +
-                    "seperated from the next by a whitespace-character, and revokes their lecture tokens " +
+                    "separated from the next by a whitespace-character, and revokes their lecture tokens " +
                     "(if they own one). They will no longer be able to post messages into the lecture chat.",
                     "\\grant <list of usernames> --  Takes a list of usernames, each one " +
-                    "seperated from the next by a whitespace-character, and grants them lecture tokens " +
+                    "separated from the next by a whitespace-character, and grants them lecture tokens " +
                     "(if they are currently listening to the lecture and do not own one). They will " +
                     "be able to post messages into the lecture chat.",
                     "\\close -- Closes the lecture and makes it inaccessible. Every current participant " +
@@ -38,10 +38,10 @@ module.exports = Object.freeze({
                 "\\log --  Will show a log of all messages send into the allchat of the room you're " +
                 "currently in, including the messageID and senderID of each message.",
                 "\\showallby <list of usernames> -- Takes a list of usernames, each one " +
-                "seperated from the next by a whitespace-character, and displays all messages " +
+                "separated from the next by a whitespace-character, and displays all messages " +
                 "send into the chat by these users.", 
                 "\\rmuser <list of usernames>  -- Takes a list of usernames, each one " +
-                "seperated from the next by a whitespace-character, and removes all of them from " +
+                "separated from the next by a whitespace-character, and removes all of them from " +
                 "the conference. They will not be able to reenter the conference.\n WARNING: It is " +
                 "not yet possible to unban a banned user!",
                 "\\rmmsg <list of msgIDs>  -- Takes a list of messageIDs, each one separated from the next " +
@@ -49,14 +49,14 @@ module.exports = Object.freeze({
                 "if they exist - from the allchat of the room you're currently in. Will also send a warning to " +
                 "the senders of the messages, reminding them to follow chat etiquette.",
                 "\\rmallby <list of usernames>  --  Takes a list of usernames, each one " +
-                "seperated from the next by a whitespace-character, and removes all messages posted " +
+                "separated from the next by a whitespace-character, and removes all messages posted " +
                 "by them into the allchat of the room you're currently in. Will also send a warning " +
                 "to these participants, reminding them to follow chat-etiquette.",
                 "\\mute <list of usernames>  --  Takes a list of usernames, each one " +
-                "seperated from the next by a whitespace-character, and mutes them, meaning they " +
+                "separated from the next by a whitespace-character, and mutes them, meaning they " +
                 "will no longer be able to post messages into the allchat.",
                 "\\unmute <list of usernames>  --  Takes a list of usernames, each one " +
-                "seperated from the next by a whitespace-character, and unmutes them, meaning they " +
+                "separated from the next by a whitespace-character, and unmutes them, meaning they " +
                 "will be able to post messages into the allchat again if they were previously muted."]
     },
     WARNING: {
