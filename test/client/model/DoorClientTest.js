91a434322983e193a65ece1f15d73c70f4eedc8f
pnschumacher <philipp.schumacher@gmx.de>
2020-08-21 17:49:42 +0200

    Adjusted DoorClientTest

--- test/client/model/DoorClientTest.js
+++ test/client/model/DoorClientTest.js
@@ -5,36 +5,43 @@ const DoorClient = require('../../../game/app/client/models/DoorClient.js');
 const TypeOfDoor = require('../../../game/app/client/shared/TypeOfDoor.js');
 const PositionClient = require('../../../game/app/client/models/PositionClient.js');
 
-//test data
-var id = TestUtil.randomInt();
-var typeOfDoor = TypeOfDoor.LEFT_DOOR;
-var name = 'door';
-var position = new PositionClient(TestUtil.randomInt(), TestUtil.randomInt());
-var isClickable = TestUtil.randomBool();
-var targetRoomID = TestUtil.randomInt();
+var id;
+var typeOfDoor;
+var name;
+var position;
+var targetRoomID;
 
 describe('DoorClient test', function() {
+
+    //test data
+    beforeEach( function () {
+        id = TestUtil.randomInt();
+        typeOfDoor = TypeOfDoor.LEFT_DOOR;
+        name = 'door';
+        position = new PositionClient(TestUtil.randomInt(), TestUtil.randomInt());
+        targetRoomID = TestUtil.randomInt();
+    });
+
     it('test constructor and getters', function() {
-        let door = new DoorClient(id, typeOfDoor, name, position, isClickable, targetRoomID);
+        let door = new DoorClient(id, typeOfDoor, name, position, targetRoomID);
 
         assert.equal(id, door.getId());
         assert.equal(typeOfDoor, door.getTypeOfDoor());
         assert.equal(name, door.getName());
         assert.equal(position, door.getMapPosition());
-        assert.equal(isClickable, door.isClickable());
         assert.equal(targetRoomID, door.getTargetRoomId());
     });
 
     it('test constructor with undefined targetRoomID', function() {
-        let door = new DoorClient(id, typeOfDoor, name, position, isClickable, undefined);
+        let door = new DoorClient(id, typeOfDoor, name, position, undefined);
         assert.equal(door.getTargetRoomId(), undefined);
     });
 
     it('test invalid input', function() {
-        expect(() => new DoorClient('id', typeOfDoor, name, mapPosition, isClickable, targetRoomID));
-        expect(() => new DoorClient(id, 'typeOfDoor', name, mapPosition, isClickable, targetRoomID));
-        expect(() => new DoorClient(id, typeOfDoor, 42, mapPosition, isClickable, targetRoomID));
-        expect(() => new DoorClient(id, typeOfDoor, name, mapPosition, 'isClickable', targetRoomID));
-        expect(() => new DoorClient(id, typeOfDoor, name, mapPosition, isClickable, 'targetRoomID'));
+        expect(() => new DoorClient('id', typeOfDoor, name, mapPosition, argetRoomID));
+        expect(() => new DoorClient(id, 'typeOfDoor', name, mapPosition, targetRoomID));
+        expect(() => new DoorClient(id, typeOfDoor, 42, mapPosition, targetRoomID));
+        expect(() => new DoorClient(id, typeOfDoor, name, 'mapPosition', targetRoomID));
+        expect(() => new DoorClient(id, typeOfDoor, name, mapPosition,'targetRoomID'));
     });
 });
