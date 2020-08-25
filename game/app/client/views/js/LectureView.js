11ef67228ae4843a9127a4bf1ddfb53dab9bfac9
pnschumacher <philipp.schumacher@gmx.de>
2020-08-25 16:26:13 +0200

    fix bug where it was not possible to write in the Lecture Chat

--- game/app/client/views/js/LectureView.js
+++ game/app/client/views/js/LectureView.js
@@ -6,11 +6,10 @@ class LectureView extends WindowView {
     #hasToken;
     #lectureId;
     #timeLeft;
-    #eventManager
+    
 
     constructor() {
         super();
-        this.#eventManager = new EventManager();
 
         this.#lectureStatus = LectureStatus.PENDING;
 
@@ -22,7 +21,7 @@ class LectureView extends WindowView {
 
                 let messageVal = $('#lectureChatInput').val().replace(/</g, "&lt;").replace(/>/g, "&gt;");
                 if (messageVal !== '') {
-                    this.#eventManager.handleLectureChatMessageInput(messageVal);
+                    new EventManager().handleLectureChatMessageInput(messageVal);
                     $('#lectureChatInput').val('');
                     $('#lectureChatInput').focus();
                 }
@@ -189,9 +188,9 @@ class LectureView extends WindowView {
             $('#lectureVideoWindow').hide();
 
             if (this.#lectureStatus === LectureStatus.RUNNING || this.#lectureStatus === LectureStatus.PENDING) {
-                this.#eventManager.handleLectureLeft(this.#lectureId, false);
+                new EventManager().handleLectureLeft(this.#lectureId, false);
             } else {
-                this.#eventManager.handleLectureLeft(this.#lectureId, true);
+                new EventManager().handleLectureLeft(this.#lectureId, true);
             }
         } else {
             $('#lectureVideoWindow').hide();
