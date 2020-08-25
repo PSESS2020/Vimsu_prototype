b62554d80d33cd558449aa85489afe6315d1540a
klaudialeo <klaudialeo@yahoo.com>
2020-08-25 16:49:18 +0200

    remove private attributes of loadingview bc its not used anywhere else in the class

--- game/app/client/views/js/LoadingView.js
+++ game/app/client/views/js/LoadingView.js
@@ -1,22 +1,16 @@
 class LoadingView extends Views {
-    #overlay;
-    #bar;
-    #status;
 
     constructor() {
         super();
-        this.#overlay = $("#overlay");
-        this.#bar = $("#progressbar");
-        this.#status = $("#progressstatus");
     }
 
     contentLoaded(totalContents, loadedContents) {
         var percentage = ((100 / totalContents * loadedContents) << 0) + "%";
-        this.#bar[0].style.width = percentage;
-        this.#status[0].innerHTML = "Loading Conference " + percentage + " . . .";
+        $("#progressbar")[0].style.width = percentage;
+        $("#progressstatus")[0].innerHTML = "Loading Conference " + percentage + " . . .";
     }
 
     doneLoading() {
-        this.#overlay[0].style.opacity = 0;
+        $("#overlay")[0].style.opacity = 0;
     }
 }
