41fa329f1db1df4f1c82860a4321e82608a4f3f2
pnschumacher <philipp.schumacher@gmx.de>
2020-08-25 20:25:53 +0200

    Added missing TypeCheckers and JSDoc Comments in AchievementDefinition

--- game/app/server/models/AchievementDefinition.js
+++ game/app/server/models/AchievementDefinition.js
@@ -11,12 +11,28 @@ module.exports = class AchievementDefinition {
     #description;
     #levels;
 
+    /**
+     * 
+     * @param {int} id 
+     * @param {TypeOfTask} taskType 
+     * @param {String} title 
+     * @param {String} icon 
+     * @param {String} description 
+     * @param {Array of Object (includes count, color, points)} levels 
+     */
     constructor(id, taskType, title, icon, description, levels) {
         TypeChecker.isInt(id);
         TypeChecker.isEnumOf(taskType, TypeOfTask);
         TypeChecker.isString(title);
         TypeChecker.isString(icon);
         TypeChecker.isString(description);
+        TypeChecker.isInstanceOf(levels, Array);
+        levels.forEach(element => {
+            TypeChecker.isInstanceOf(element, Object);
+            TypeChecker.isInt(element.count);
+            TypeChecker.isString(element.color);
+            TypeChecker.isInt(element.points);
+        });
 
         this.#id = id;
         this.#taskType = taskType;
@@ -50,7 +66,12 @@ module.exports = class AchievementDefinition {
         return this.#levels;
     }
 
+    /**
+     * 
+     * @param {int} currentLevel 
+     */
     computeAchievement(currentLevel) {
+        TypeChecker.isInt(currentLevel);
         var color = (currentLevel != 0) ? this.#levels[currentLevel - 1].color : 'darkslategray';
         var awardPoints = (currentLevel != 0) ? this.#levels[currentLevel - 1].points : 0;
         var nextCount = (currentLevel != this.#levels.length) ? this.#levels[currentLevel].count : undefined;
