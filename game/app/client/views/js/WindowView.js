fd28517669d24cae776faaf1e55457d611bdaa4b
klaudialeo <klaudialeo@yahoo.com>
2020-08-25 23:09:50 +0200

    add missing error message on instantiating windowview

--- game/app/client/views/js/WindowView.js
+++ game/app/client/views/js/WindowView.js
@@ -2,6 +2,10 @@ class WindowView extends Views {
 
     constructor() {
         super();
+
+        if (new.target === WindowView) {
+            throw new Error("Cannot construct abstract WindowView instances directly");
+        }
     }
 
     draw() {
