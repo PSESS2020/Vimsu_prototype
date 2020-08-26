1fd6f492a130a26bc0aa9f097555d6f7d84b269c
klaudialeo <klaudialeo@yahoo.com>
2020-08-26 17:40:29 +0200

    add missing JSDocs comments to index.js

--- game/app/client/index.js
+++ game/app/client/index.js
@@ -3,8 +3,13 @@ const GAME_HEIGHT = GameConfig.CTX_HEIGHT;
 let ctx_map, ctx_avatar, ctx_ui;
 let clientController;
 
+/* Initializes canvas and ClientController on window onload */
 window.onload = this.init();
 
+/**
+ * Initializes canvas and ClientController for the first time.
+ * Also calls game loop to update frame constantly
+ */
 function init() {
   ctx_map = document.getElementById("mapCanvas").getContext("2d");
   ctx_avatar = document.getElementById("avatarCanvas").getContext("2d");
@@ -25,13 +30,20 @@ function init() {
   window.requestAnimationFrame(gameLoop);
 }
 
+/**
+ * Updates game constantly through a game loop
+ * 
+ * @param {number} timeStamp 
+ */
 function gameLoop(timeStamp) {
-    //Clear canvas
-    ctx_avatar.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-    
-    // Perform the drawing operation
-    clientController.updateGame(timeStamp);
+  TypeChecker.isNumber(timeStamp);
 
-    // The loop function has reached it's end. Keep requesting new frames
-    window.requestAnimationFrame(gameLoop);
+  //Clear canvas
+  ctx_avatar.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+  // Perform the drawing operation
+  clientController.updateGame(timeStamp);
+
+  // The loop function has reached it's end. Keep requesting new frames
+  window.requestAnimationFrame(gameLoop);
 }
