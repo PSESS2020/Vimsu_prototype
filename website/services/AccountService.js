30250d7d2042083e7dfdda17f2e5cbcc5048e420
klaudialeo <klaudialeo@yahoo.com>
2020-08-26 10:51:10 +0200

    add JSDoc and typechecker of AccountService

--- website/services/AccountService.js
+++ website/services/AccountService.js
@@ -2,11 +2,18 @@ const TypeChecker = require('../../game/app/client/shared/TypeChecker.js');
 const Account = require('../models/Account')
 const ObjectId = require('mongodb').ObjectID;
 const passwordHash = require('password-hash');
+const db = require('../../config/db');
 
 module.exports = class AccountService {
 
+    /**
+     * 
+     * @param {String} username 
+     * @param {db} vimsudb 
+     */
     static isUsernameValid(username, vimsudb) {
         TypeChecker.isString(username);
+        TypeChecker.isInstanceOf(vimsudb, db)
 
         return vimsudb.findInCollection("accounts", { username: username }, { username: username }).then(results => {
             if (results.length > 0) {
@@ -19,9 +26,14 @@ module.exports = class AccountService {
 
     }
 
+    /**
+     * 
+     * @param {String} email 
+     * @param {db} vimsudb 
+     */
     static isEmailValid(email, vimsudb) {
         TypeChecker.isString(email);
-
+        TypeChecker.isInstanceOf(vimsudb, db)
 
         return vimsudb.findInCollection("accounts", { email: email }, { email: email }).then(results => {
             if (results.length > 0) {
@@ -34,7 +46,28 @@ module.exports = class AccountService {
 
     }
 
+    /**
+     * 
+     * @param {String} username 
+     * @param {String} title 
+     * @param {String} surname 
+     * @param {String} forename 
+     * @param {String} job 
+     * @param {String} company 
+     * @param {String} email 
+     * @param {String} password 
+     * @param {db} vimsudb 
+     */
     static createAccount(username, title, surname, forename, job, company, email, password, vimsudb) {
+        TypeChecker.isString(username);
+        TypeChecker.isString(title);
+        TypeChecker.isString(surname);
+        TypeChecker.isString(forename);
+        TypeChecker.isString(job);
+        TypeChecker.isString(company);
+        TypeChecker.isString(email);
+        TypeChecker.isString(password);
+        TypeChecker.isInstanceOf(vimsudb, db);
 
         var accountId = new ObjectId().toString();
         var account = new Account(accountId, username, title, surname, forename, job, company, email);
@@ -59,12 +92,16 @@ module.exports = class AccountService {
 
     }
 
+    /**
+     * 
+     * @param {String} accountId 
+     * @param {db} vimsudb 
+     */
     static getAccountById(accountId, vimsudb) {
         TypeChecker.isString(accountId);
-
+        TypeChecker.isInstanceOf(vimsudb, db);
 
         return vimsudb.findOneInCollection("accounts", { accountId: accountId }, "").then(user => {
-
             if (user) {
                 return user;
             }
@@ -77,9 +114,14 @@ module.exports = class AccountService {
 
     }
 
+    /**
+     * 
+     * @param {String} username 
+     * @param {db} vimsudb 
+     */
     static #getAccountByUsername = function(username, vimsudb) {
         TypeChecker.isString(username);
-
+        TypeChecker.isInstanceOf(vimsudb, db);
 
         return vimsudb.findOneInCollection("accounts", { username: username }, "").then(user => {
 
@@ -95,27 +137,14 @@ module.exports = class AccountService {
 
     }
 
-    static getAccountID(username, vimsudb) {
-        TypeChecker.isString(username);
-
-
-        return vimsudb.findOneInCollection("accounts", { username: username }, { accountId: 1 }).then(user => {
-
-            if (user) {
-                return user.accountId;
-            }
-            else {
-                return false;
-            }
-        }).catch(err => {
-            console.error(err);
-        })
-
-    }
-
+    /**
+     * 
+     * @param {String} accountId 
+     * @param {db} vimsudb 
+     */
     static getAccountUsername(accountId, vimsudb) {
         TypeChecker.isString(accountId);
-
+        TypeChecker.isInstanceOf(vimsudb, db);
 
         return vimsudb.findOneInCollection("accounts", { accountId: accountId }, { username: 1 }).then(user => {
 
@@ -131,98 +160,29 @@ module.exports = class AccountService {
 
     }
 
-    static getAccountName(accountId, vimsudb) {
-        TypeChecker.isString(accountId);
-
-
-        return vimsudb.findOneInCollection("accounts", { accountId: accountId }, { title: 1, surname: 1, forename: 1 }).then(user => {
-
-            if (user) {
-                var name = user.title + " " + user.forename + " " + user.surname;
-                return name;
-            }
-            else {
-                return false;
-            }
-        }).catch(err => {
-            console.error(err);
-        })
-
-    }
-
-    static getAccountTitle(accountId, vimsudb) {
-        TypeChecker.isString(accountId);
-
-
-        return vimsudb.findOneInCollection("accounts", { accountId: accountId }, { title: 1 }).then(user => {
-
-            if (user) {
-                return user.title;
-            }
-            else {
-                return false;
-            }
-        }).catch(err => {
-            console.error(err);
-        })
-
-    }
-
-    static getAccountSurname(accountId, vimsudb) {
-        TypeChecker.isString(accountId);
-
-
-        return vimsudb.findOneInCollection("accounts", { accountId: accountId }, { surname: 1 }).then(user => {
-
-            if (user) {
-                return user.surname;
-            }
-            else {
-                return false;
-            }
-        }).catch(err => {
-            console.error(err)
-        });
-
-    }
-
-    static getAccountForename(accountId, vimsudb) {
-        TypeChecker.isString(accountId);
-
-
-        return vimsudb.findOneInCollection("accounts", { accountId: accountId }, { forename: 1 }).then(user => {
-
-            if (user) {
-                return user.forename;
-            }
-            else {
-                return false;
-            }
-        }).catch(err => {
-            console.error(err)
-        })
-
-    }
-
-    static getAccountEmail(accountId, vimsudb) {
-        TypeChecker.isString(accountId);
-
-
-        return vimsudb.findOneInCollection("accounts", { accountId: accountId }, { email: 1 }).then(user => {
-
-            if (user) {
-                return user.email;
-            }
-            else {
-                return false;
-            }
-        }).catch(err => {
-            console.error(err)
-        });
-
-    }
-
+    /**
+     * 
+     * @param {String} accountId
+     * @param {String} username 
+     * @param {String} newTitle 
+     * @param {String} newSurname 
+     * @param {String} newForename 
+     * @param {String} newJob 
+     * @param {String} newCompany 
+     * @param {String} email 
+     * @param {db} vimsudb 
+     */
     static updateAccountData(accountId, username, newTitle, newSurname, newForename, newJob, newCompany, email, vimsudb) {
+        TypeChecker.isString(accountId);
+        TypeChecker.isString(username);
+        TypeChecker.isString(newTitle);
+        TypeChecker.isString(newSurname);
+        TypeChecker.isString(newForename);
+        TypeChecker.isString(newJob);
+        TypeChecker.isString(newCompany);
+        TypeChecker.isString(email);
+        TypeChecker.isInstanceOf(vimsudb, db);
+
         var account = new Account(accountId, username, newTitle, newSurname, newForename, newJob, newCompany, email)
 
         return vimsudb.updateOneToCollection("accounts", { accountId: accountId }, { title: newTitle, surname: newSurname, forename: newForename, job: newJob, company: newCompany }).then(res => {
@@ -231,75 +191,18 @@ module.exports = class AccountService {
         }).catch(err => {
             console.error(err)
         });
-
-
     }
 
-    static updateUsername(accountId, newUsername, vimsudb) {
-        TypeChecker.isString(accountId);
-        TypeChecker.isString(newUsername);
-
-
-        return this.isUsernameValid(newUsername).then(res => {
-            if (res) {
-                return vimsudb.updateOneToCollection("accounts", { accountId: accountId }, { username: newUsername })
-                .catch(err => {
-                    console.error(err)
-                });
-
-            }
-            else {
-                return false;
-            }
-        })
-    }
-
-    static updateEmail(accountId, newEmail, vimsudb) {
-        TypeChecker.isString(accountId);
-        TypeChecker.isString(newEmail);
-
-        return this.isEmailValid(newEmail).then(res => {
-            if (res) {
-
-                return vimsudb.updateOneToCollection("accounts", { accountId: accountId }, { email: newEmail })
-                .catch(err => {
-                    console.error(err)
-                });
-
-            }
-            else {
-                return false;
-            }
-        })
-    }
-
-    static updatePassword(accountId, newPassword, vimsudb) {
-        TypeChecker.isString(accountId);
-        TypeChecker.isString(newPassword);
-        var newPasswordHash = passwordHash.generate(newPassword);
-
-        return vimsudb.updateOneToCollection("accounts", { accountId: accountId }, { passwordHash: newPasswordHash })
-        .catch(err => {
-            console.error(err)
-        });
-
-    }
-
-    static deleteAccount(accountId, vimsudb) {
-        TypeChecker.isString(accountId);
-
-        return vimsudb.deleteOneFromCollection("accounts", { accountId: accountId }).then(res => {
-            console.log("account with " + accountId + " is deleted")
-        }).catch(err => {
-            console.error(err)
-        });
-
-
-    }
-
+    /**
+     * 
+     * @param {String} username 
+     * @param {String} password 
+     * @param {db} vimsudb 
+     */
     static verifyLoginData(username, password, vimsudb) {
         TypeChecker.isString(username);
         TypeChecker.isString(password);
+        TypeChecker.isInstanceOf(vimsudb, db);
 
         return this.#getAccountByUsername(username, vimsudb).then(user => {
             if (user && passwordHash.verify(password, user.passwordHash)) {
