69e00b1e30d92e93698ab0e7a998610ee43979f2
klaudialeo <klaudialeo@yahoo.com>
2020-08-26 10:11:44 +0200

    add JSDoc and typechecker of LectureContext

--- game/app/server/models/LectureContext.js
+++ game/app/server/models/LectureContext.js
@@ -2,15 +2,22 @@ const CommandContext = require('./CommandContext.js');
 const TypeChecker = require('../../client/shared/TypeChecker.js');
 const Messages = require('../utils/Messages.js');
 const Lecture = require('./Lecture.js');
+const ServerController = require('../controller/ServerController');
 
 module.exports = class LectureContext extends CommandContext {
     
     #serverController
     #contextObject
     
+    /**
+     * 
+     * @param {ServerController} serverController 
+     * @param {Lecture} lecture 
+     */
     constructor(serverController, lecture) {
         super();
-        //TypeChecker.isInstanceOf(serverController, ServerController);
+
+        TypeChecker.isInstanceOf(serverController, ServerController);
         TypeChecker.isInstanceOf(lecture, Lecture);
         this.#serverController = serverController;
         this.#contextObject = lecture;
@@ -33,7 +40,13 @@ module.exports = class LectureContext extends CommandContext {
         this.#serverController.emitEventIn(this.#contextObject.getId(), 'updateLectureChat', this.getMessages());
     };
     
+    /**
+     * 
+     * @param {String} userToRemove username of the user to remove
+     */
     removeUser(userToRemove) {
+        TypeChecker.isString(userToRemove);
+
         var ppantId = this.#serverController.getIdOf(userToRemove);
         if (ppantId !== undefined && this.#contextObject.hasPPant(ppantId)) {
             this.#removeByID(ppantId, Messages.REMOVAL);
@@ -50,7 +63,13 @@ module.exports = class LectureContext extends CommandContext {
         }
     };
     
+    /**
+     * 
+     * @param {String} userToMute username of the user to mute
+     */
     muteUser(userToMute) {
+        TypeChecker.isString(userToMute);
+
         //it is not possible to mute the orator
         if (userToMute !== this.#contextObject.getOratorUsername()) {
             var ppantID = this.#serverController.getIdOf(userToMute);
@@ -63,7 +82,13 @@ module.exports = class LectureContext extends CommandContext {
         }
     };
     
+    /**
+     * 
+     * @param {String} userToUnmute username of the user to unmute
+     */
     unmuteUser(userToUnmute) {
+        TypeChecker.isString(userToUnmute);
+
         var ppantID = this.#serverController.getIdOf(userToUnmute);
         // If the uses did not previously posess a token, we need to inform him he now does
         if(ppantID !== undefined && this.#contextObject.grantToken(ppantID)) {
@@ -73,7 +98,15 @@ module.exports = class LectureContext extends CommandContext {
         };
     };
     
+    /**
+     * 
+     * @param {String} ppantId 
+     * @param {Messages} message 
+     */
     #removeByID = function(ppantId, message) {
+        TypeChecker.isString(ppantId);
+        TypeChecker.isEnumOf(message, Messages);
+        
         var socketClient = this.#serverController.getSocketObject(this.#serverController.getSocketId(ppantId));
         this.#contextObject.leave(ppantId);
         this.#contextObject.revokeToken(ppantId);
