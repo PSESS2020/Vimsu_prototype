b6fedd19a3ca84406835d7ad82493d70d4f0a248
klaudialeo <klaudialeo@yahoo.com>
2020-08-26 12:47:34 +0200

    add JSDoc and typechecker of FileSystem

--- config/FileSystem.js
+++ config/FileSystem.js
@@ -1,20 +1,39 @@
 const fs = require('fs');
 const mkdirp = require('mkdirp');
 const rimraf = require('rimraf');
+const TypeChecker = require('../game/app/client/shared/TypeChecker');
 
 module.exports = class FileSystem {
 
+    /**
+     * 
+     * @param {String} dir 
+     */
     static createDirectory(dir) {
+        TypeChecker.isString(dir);
         mkdirp.sync(dir);
     }
 
+    /**
+     * 
+     * @param {String} dir 
+     */
     static deleteDirectory(dir) {
+        TypeChecker.isString(dir);
+
         rimraf(dir, function () { 
             console.log(`${dir} directory is deleted`); 
         });
     }
 
+    /**
+     * 
+     * @param {Object} file 
+     * @param {String} dir 
+     */
     static moveFile(file, dir) {
+        TypeChecker.isString(dir);
+
         return new Promise((resolve, reject) => {
             this.createDirectory(dir);
 
@@ -27,7 +46,12 @@ module.exports = class FileSystem {
         })
     }
 
+    /**
+     * 
+     * @param {String} filePath 
+     */
     static createReadStream(filePath) {
+        TypeChecker.isString(filePath);
         return fs.createReadStream(filePath);
     }
 }
