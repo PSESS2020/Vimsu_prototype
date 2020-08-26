1267e6f263a9eee0b9162deb22fc7d02d826155a
klaudialeo <klaudialeo@yahoo.com>
2020-08-26 09:53:02 +0200

    add JSDoc and typechecker of allchatcontext

--- game/app/server/models/AllchatContext.js
+++ game/app/server/models/AllchatContext.js
@@ -2,15 +2,22 @@ const CommandContext = require('./CommandContext.js');
 const TypeChecker = require('../../client/shared/TypeChecker.js');
 const Messages = require('../utils/Messages.js');
 const Room = require('./Room.js');
+const ServerController = require('../controller/ServerController.js');
 
 module.exports = class AllchatContext extends CommandContext {
     
     #serverController
     #contextObject
     
+    /**
+     * 
+     * @param {ServerController} serverController 
+     * @param {Room} room 
+     */
     constructor(serverController, room) {
         super();
-        //TypeChecker.isInstanceOf(serverController, ServerController);
+
+        TypeChecker.isInstanceOf(serverController, ServerController);
         TypeChecker.isInstanceOf(room, Room);
         this.#serverController = serverController;
         this.#contextObject = room;
@@ -32,14 +39,18 @@ module.exports = class AllchatContext extends CommandContext {
         this.#serverController.emitEventIn(this.#contextObject.getRoomId(), 'initAllchat', this.getMessages());
     };
     
+    /**
+     * 
+     * @param {String} userToRemove username of the user to remove
+     */
     removeUser(userToRemove) {
         // removes player(s) from conference
+
+        TypeChecker.isString(userToRemove);
         
         var ppantID = this.#serverController.getIdOf(userToRemove);
         if (ppantID !== undefined) {
             var socket = this.#serverController.getSocketObject(this.#serverController.getSocketId(ppantID));
-            var accountId = socket.request.session.accountId;
-
 
             /* First, it gets the socket object corresponding to player that
             * is supposed to be removed from the game. 
@@ -76,7 +87,13 @@ module.exports = class AllchatContext extends CommandContext {
         return; // does nothing
     };
     
+    /**
+     * 
+     * @param {String} userToMute username of the user to mute
+     */
     muteUser(userToMute) {
+        TypeChecker.isString(userToMute);
+
         var ppantID = this.#serverController.getIdOf(userToMute);
         if (ppantID !== undefined) {
             var socket = this.#serverController.getSocketObject(this.#serverController.getSocketId(ppantID));
@@ -88,7 +105,13 @@ module.exports = class AllchatContext extends CommandContext {
         }
     };
     
+    /**
+     * 
+     * @param {String} userToUnmute username of the user to unmute
+     */
     unmuteUser(userToUnmute) {        
+        TypeChecker.isString(userToUnmute);
+        
         var ppantID = this.#serverController.getIdOf(userToUnmute);
         if (ppantID !== undefined) {
             var socket = this.#serverController.getSocketObject(this.#serverController.getSocketId(ppantID));
