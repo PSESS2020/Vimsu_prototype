5edd9146f2234772e821fd2424230586256b398c
klaudialeo <klaudialeo@yahoo.com>
2020-08-26 12:00:06 +0200

    add JSDoc and typechecker of SpriteSheet

--- game/app/client/utils/SpriteSheet.js
+++ game/app/client/utils/SpriteSheet.js
@@ -1,5 +1,16 @@
 class SpriteSheet {
+
+    /**
+     * 
+     * @param {String} path 
+     * @param {number} frameWidth 
+     * @param {number} frameHeight 
+     */
     constructor(path, frameWidth, frameHeight) {
+        TypeChecker.isString(path);
+        TypeChecker.isInt(frameWidth);
+        TypeChecker.isInt(frameHeight);
+
         this.image = new Image();
         this.path = path;
         this.frameWidth = frameWidth;
