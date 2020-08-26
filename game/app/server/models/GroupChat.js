85418c734e88da19db5dce0ee167e765b487c660
klaudialeo <klaudialeo@yahoo.com>
2020-08-26 18:26:24 +0200

    add missing JSDocs comments to GroupChat

--- game/app/server/models/GroupChat.js
+++ game/app/server/models/GroupChat.js
@@ -9,14 +9,15 @@ module.exports = class GroupChat extends Chat {
     #ownerId;
 
     /**
+     * @constructor Creates group chat instance
      * 
-     * @param {String} chatId 
-     * @param {String} ownerId 
-     * @param {String} chatName 
-     * @param {?String[]} participantList 
-     * @param {Message[]} messageList 
-     * @param {number} maxParticipants 
-     * @param {number} maxNumMessages 
+     * @param {String} chatId chat ID
+     * @param {String} ownerId group chat owner ID
+     * @param {String} chatName group chat name
+     * @param {?String[]} participantList group chat participant list
+     * @param {Message[]} messageList group chat message list
+     * @param {number} maxParticipants max participants of group chat
+     * @param {number} maxNumMessages max number of messages of group chat
      */
     constructor(chatId, ownerId, chatName, participantList, messageList, maxParticipants, maxNumMessages) {
         super(chatId, participantList, messageList, maxNumMessages);
@@ -30,17 +31,28 @@ module.exports = class GroupChat extends Chat {
         this.#maxParticipants = maxParticipants;
     }
 
+    /**
+     * Gets group chat name
+     * 
+     * @return chatName
+     */
     getChatName() {
         return this.#chatName;
     }
 
+    /**
+     * Gets group chat owner ID
+     * 
+     * @return ownerId
+     */
     getOwnerId() {
         return this.#ownerId;
     }
 
     /**
+     * Sets group chat name
      * 
-     * @param {String} chatName 
+     * @param {String} chatName group chat name
      */
     setChatName(chatName) {
         TypeChecker.isString(chatName);
@@ -51,7 +63,7 @@ module.exports = class GroupChat extends Chat {
      * Adds a message to the message list.
      * If message list is full then the half of the message list gets deleted.
      * 
-     * @param {Message} msg 
+     * @param {Message} msg group chat message
      */
     addMessage(msg) {
         TypeChecker.isInstanceOf(msg, Message);
@@ -65,8 +77,11 @@ module.exports = class GroupChat extends Chat {
     }
 
     /**
-     * Addds a participant to the participant list if maximum amount of members is not exceeded.
-     * @param {String} participantId 
+     * Adds a participant to the participant list if maximum amount of members is not exceeded.
+     * 
+     * @param {String} participantId participant ID
+     * 
+     * @return true if successfully added, otherwise false
      */
     addParticipant(participantId) {
         TypeChecker.isString(participantId);
@@ -75,7 +90,6 @@ module.exports = class GroupChat extends Chat {
         if (ppantList.length >= this.#maxParticipants) {
             return false;
         } else {
-
             if (!ppantList.includes(participantId)) {
                 ppantList.push(participantId);
                 return true;
@@ -85,13 +99,13 @@ module.exports = class GroupChat extends Chat {
     }
 
     /**
+     * Remove participant from the group chat
      * 
-     * @param {String} participantId 
+     * @param {String} participantId participant ID
      */
     removeParticipant(participantId) {
         TypeChecker.isString(participantId);
 
         super.removeParticipant(participantId);
     }
-
 }
