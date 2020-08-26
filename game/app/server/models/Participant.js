ce320b3b3eae64186f2f64acbe249ef0a63f2275
klaudialeo <klaudialeo@yahoo.com>
2020-08-26 19:00:50 +0200

    add missing JSDocs comments to Participant

--- game/app/server/models/Participant.js
+++ game/app/server/models/Participant.js
@@ -7,6 +7,7 @@ const Achievement = require('./Achievement.js');
 const Chat = require('./Chat.js');
 const Task = require('./Task.js');
 const OneToOneChat = require('./OneToOneChat.js');
+const TypeOfTask = require('../utils/TypeOfTask')
 
 module.exports = class Participant {
 
@@ -26,20 +27,21 @@ module.exports = class Participant {
     #isVisible;
 
     /**
+     * @constructor Creates a participant instance
      * 
-     * @param {String} id 
-     * @param {String} accountId 
-     * @param {BusinessCard} businessCard 
-     * @param {Position} position 
-     * @param {Direction} direction 
-     * @param {FriendList} friendList 
-     * @param {FriendList} receivedRequestList 
-     * @param {FriendList} sentRequestList 
-     * @param {Achievement[]} achievements 
-     * @param {Task[]} taskMapping
-     * @param {boolean} isMod 
-     * @param {number} awardPoints
-     * @param {Chat[]} chatList 
+     * @param {String} id participant ID
+     * @param {String} accountId account ID
+     * @param {BusinessCard} businessCard business card
+     * @param {Position} position participant position
+     * @param {Direction} direction avatar direction
+     * @param {FriendList} friendList list of friends
+     * @param {FriendList} receivedRequestList list of received friend requests
+     * @param {FriendList} sentRequestList list of sent friend requests
+     * @param {Achievement[]} achievements list of achievements
+     * @param {Task[]} taskMapping list of tasks and its counts
+     * @param {boolean} isMod moderator status
+     * @param {number} awardPoints participant's points
+     * @param {Chat[]} chatList list of chats
      */
     constructor(id, accountId, businessCard, position, direction, friendList, receivedRequestList, sentRequestList, achievements, taskMapping, isMod, awardPoints, chatList) {
         //Typechecking
@@ -81,57 +83,118 @@ module.exports = class Participant {
         this.#isVisible = true;
     }
 
+    /**
+     * Gets participant ID
+     * 
+     * @return id
+     */
     getId() {
         return this.#id;
     }
 
+    /**
+     * Gets participant's position
+     * 
+     * @return position
+     */
     getPosition() {
         return this.#position;
     }
 
+    /**
+     * Gets avatar's direction
+     * 
+     * @return direction
+     */
     getDirection() {
         return this.#direction;
     }
 
+    /**
+     * Gets account ID
+     * 
+     * @return accountId
+     */
     getAccountId() {
         return this.#accountId;
     }
 
+    /**
+     * Gets business card
+     * 
+     * @return businessCard
+     */
     getBusinessCard() {
         return this.#businessCard;
     }
 
+    /**
+     * Gets list of friends
+     * 
+     * @return friendList
+     */
     getFriendList() {
         return this.#friendList;
     }
 
+    /**
+     * Gets list of received friend requests
+     * 
+     * @return receivedRequestList
+     */
     getReceivedRequestList() {
         return this.#receivedRequestList;
     }
 
+    /**
+     * Gets list of sent friend requests
+     * 
+     * @return sentRequestList
+     */
     getSentRequestList() {
         return this.#sentRequestList;
     }
 
+    /**
+     * Gets list of achievements
+     * 
+     * @return achievements
+     */
     getAchievements() {
         return this.#achievements;
     }
 
+    /**
+     * Gets avatar's visibility 
+     * 
+     * @return true if visible, otherwise false
+     */
     getIsVisible() {
         return this.#isVisible;
     }
 
+    /**
+     * Gets moderator status
+     * 
+     * @return true if moderator, otherwise false
+     */
     getIsModerator() {
         return this.#isMod;
     }
 
+    /**
+     * Gets chat list
+     * 
+     * @return chatList
+     */
     getChatList() {
         return this.#chatList;
     }
 
     /**
+     * Adds chat to the chat list
      * 
-     * @param {Chat} chat 
+     * @param {Chat} chat chat instance
      */
     addChat(chat) {
         TypeChecker.isInstanceOf(chat, Chat);
@@ -141,8 +204,9 @@ module.exports = class Participant {
     }
 
     /**
+     * Sets avatar's position
      * 
-     * @param {Position} position 
+     * @param {Position} position position
      */
     setPosition(position) {
         TypeChecker.isInstanceOf(position, Position);
@@ -150,8 +214,9 @@ module.exports = class Participant {
     }
 
     /**
+     * Sets avatar's direction
      * 
-     * @param {Direction} direction 
+     * @param {Direction} direction direction
      */
     setDirection(direction) {
         TypeChecker.isEnumOf(direction, Direction);
@@ -159,8 +224,9 @@ module.exports = class Participant {
     }
 
     /**
+     * Sets visibility
      * 
-     * @param {boolean} isVisible 
+     * @param {boolean} isVisible visibility
      */
     setIsVisible(isVisible) {
         TypeChecker.isBoolean(isVisible);
@@ -168,8 +234,10 @@ module.exports = class Participant {
     }
 
     /**
+     * Adds ppant to list of sent friend request.
      * Method called, when this ppant sends a friend request to ppant with this businessCard
-     * @param {BusinessCard} businessCard 
+     * 
+     * @param {BusinessCard} businessCard business card
      */
     addSentFriendRequest(businessCard) {
         TypeChecker.isInstanceOf(businessCard, BusinessCard);
@@ -180,8 +248,9 @@ module.exports = class Participant {
     }
 
     /**
-     * Called when outgoing friend request to ppant with ppantId was accepted
-     * @param {String} ppantId 
+     * Called when the outgoing friend request to ppant with ppantId was accepted
+     * 
+     * @param {String} ppantId participant ID
      */
     sentFriendRequestAccepted(ppantId) {
         TypeChecker.isString(ppantId);
@@ -194,7 +263,8 @@ module.exports = class Participant {
 
     /**
      * Called when outgoing friend request to ppant with ppantId was declined
-     * @param {String} ppantId 
+     * 
+     * @param {String} ppantId participant ID
      */
     sentFriendRequestDeclined(ppantId) {
         TypeChecker.isString(ppantId);
@@ -204,8 +274,9 @@ module.exports = class Participant {
     }
 
     /**
-     * Method called to add a FriendRequest
-     * @param {BusinessCard} businessCard 
+     * Adds a friend request to the received friend request list
+     * 
+     * @param {BusinessCard} businessCard business card
      */
     addFriendRequest(businessCard) {
         TypeChecker.isInstanceOf(businessCard, BusinessCard);
@@ -217,7 +288,8 @@ module.exports = class Participant {
 
     /**
      * Accept FriendRequest from ppantId, if a request exists
-     * @param {String} ppantId 
+     * 
+     * @param {String} ppantId participant ID
      */
     acceptFriendRequest(ppantId) {
         TypeChecker.isString(ppantId);
@@ -230,7 +302,8 @@ module.exports = class Participant {
 
     /**
      * Declines FriendRequest from ppantId, if a request exists
-     * @param {String} ppantId 
+     * 
+     * @param {String} ppantId participant ID
      */
     declineFriendRequest(ppantId) {
         TypeChecker.isString(ppantId);
@@ -241,7 +314,8 @@ module.exports = class Participant {
 
     /**
      * Removes ppant with ppantId from FriendList, if he is part of it
-     * @param {String} ppantId 
+     * 
+     * @param {String} ppantId participant ID
      */
     removeFriend(ppantId) {
         TypeChecker.isString(ppantId);
@@ -251,20 +325,21 @@ module.exports = class Participant {
     }
 
     /**
+     * Checks if participant has a friend with a participant with ppantId
      * 
-     * @param {?String} ppantId 
+     * @param {?String} ppantId participant ID
      */
     hasFriend(ppantId) {
         if (ppantId) {
             TypeChecker.isString(ppantId);
-
             return this.#friendList.includes(ppantId) // TODO: method does more than the name says it does
         }
     }
 
     /**
+     * Checks if participant has sent/received a friend request to/from a participant with ppantId
      * 
-     * @param {?String} ppantId 
+     * @param {?String} ppantId participant ID
      */
     hasSentFriendRequest(ppantId) {
         if (ppantId) {
@@ -275,8 +350,9 @@ module.exports = class Participant {
     }
 
     /**
+     * Removes chat from the chat list
      * 
-     * @param {String} chatId 
+     * @param {String} chatId chat ID
      */
     removeChat(chatId) {
         TypeChecker.isString(chatId);
@@ -290,17 +366,29 @@ module.exports = class Participant {
         });
     }
 
+    /**
+     * Gets tasks and its count
+     * 
+     * @return taskTypeMapping
+     */
     getTaskTypeMappingCounts() {
         return this.#taskTypeMapping;
     }
 
+    /**
+     * Gets a task based on the task type
+     * @param {TypeOfTask} taskType task type
+     * 
+     * @return task
+     */
     getTaskTypeMappingCount(taskType) {
         return this.#taskTypeMapping[taskType];
     }
 
     /**
+     * Called if a task is fulfilled to increase the counter and the points
      * 
-     * @param {Task} task 
+     * @param {Task} task task instance
      */
     addTask(task) {
         TypeChecker.isInstanceOf(task, Task);
@@ -311,8 +399,9 @@ module.exports = class Participant {
     }
 
     /**
+     * Adds points of participant
      * 
-     * @param {number} awardPoints 
+     * @param {number} awardPoints points
      */
     addAwardPoints(awardPoints) {
         TypeChecker.isInt(awardPoints);
@@ -320,8 +409,9 @@ module.exports = class Participant {
     }
 
     /**
+     * Sets achievements
      * 
-     * @param {Achievement[]} achievements 
+     * @param {Achievement[]} achievements achievements
      */
     setAchievements(achievements) {
         TypeChecker.isInstanceOf(achievements, Array);
@@ -332,13 +422,21 @@ module.exports = class Participant {
         this.#achievements = achievements;
     }
 
+    /**
+     * Gets participant's points
+     * 
+     * @return points
+     */
     getAwardPoints() {
         return this.#awardPoints;
     }
 
     /**
+     * Gets a chat from the chat list
      * 
-     * @param {String} chatId 
+     * @param {String} chatId chat ID
+     * 
+     * @return chat
      */
     getChat(chatId) {
         TypeChecker.isString(chatId);
@@ -351,8 +449,11 @@ module.exports = class Participant {
     };
 
     /**
+     * Checks if participant is member of chat with this chatId
      * 
-     * @param {String} chatId 
+     * @param {String} chatId chat ID
+     * 
+     * @return true if so, otherwise false
      */
     isMemberOfChat(chatId) {
         TypeChecker.isString(chatId);
@@ -366,8 +467,9 @@ module.exports = class Participant {
     };
 
     /**
+     * Adds an achievement to the list of achievements
      * 
-     * @param {Achievement} achievement 
+     * @param {Achievement} achievement achievement
      */
     addAchievement(achievement) {
         TypeChecker.isInstanceOf(achievement, Achievement);
@@ -375,8 +477,9 @@ module.exports = class Participant {
     }
 
     /**
+     * Removes an achievement from the list of achievements
      * 
-     * @param {number} achievementId 
+     * @param {number} achievementId achievement ID
      */
     removeAchievement(achievementId) {
         TypeChecker.isInt(achievementId);
@@ -393,7 +496,9 @@ module.exports = class Participant {
     /**
      * Check if this ppant has a 1:1 chat with chatPartnerID
      * 
-     * @param {String} chatPartnerID 
+     * @param {String} chatPartnerID chat partner ID
+     * 
+     * @return true if so, otherwise false
      */
     hasChatWith(chatPartnerID) {
         TypeChecker.isString(chatPartnerID);
