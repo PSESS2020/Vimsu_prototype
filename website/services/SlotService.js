ef5b3847b74536853e92e51de58b8c26db4ef502
klaudialeo <klaudialeo@yahoo.com>
2020-08-26 10:56:06 +0200

    add JSDoc and typechecker of SlotService

--- website/services/SlotService.js
+++ website/services/SlotService.js
@@ -2,9 +2,19 @@ const FileSystem = require('../../config/FileSystem');
 const ObjectId = require('mongodb').ObjectID;
 const TypeChecker = require('../../game/app/client/shared/TypeChecker.js');
 const Slot = require('../models/Slot')
+const blobClient = require('../../config/blob');
+const db = require('../../config/db');
 
 module.exports = class SlotService {
+
+    /**
+     * 
+     * @param {Object} video 
+     * @param {blobClient} blob 
+     */
     static storeVideo(video, blob) {
+        TypeChecker.isInstanceOf(blob, blobClient);
+
         var dir = __dirname + "/upload/";
 
         return FileSystem.moveFile(video, dir).then(res => {
@@ -21,11 +31,30 @@ module.exports = class SlotService {
         }).catch(err => {
             console.error(err);
         })
-
     }
 
+    /**
+     * 
+     * @param {String} videoId 
+     * @param {number} duration
+     * @param {String} conferenceId
+     * @param {String} title 
+     * @param {String} remarks 
+     * @param {Date} startingTime 
+     * @param {String} oratorId
+     * @param {number} maxParticipants 
+     * @param {db} vimsudb
+     */
     static createSlot(videoId, duration, conferenceId, title, remarks, startingTime, oratorId, maxParticipants, vimsudb) {
-
+        TypeChecker.isString(title);
+        TypeChecker.isString(videoId);
+        TypeChecker.isNumber(duration);
+        TypeChecker.isString(conferenceId);
+        TypeChecker.isString(remarks);
+        TypeChecker.isDate(startingTime);
+        TypeChecker.isString(oratorId);
+        TypeChecker.isInt(maxParticipants);
+        TypeChecker.isInstanceOf(vimsudb, db);
 
         var id = new ObjectId().toString();
         var slot = new Slot(id, title, conferenceId, videoId, duration, remarks, startingTime, oratorId, maxParticipants);
@@ -44,19 +73,30 @@ module.exports = class SlotService {
         }
 
         return vimsudb.insertOneToCollection("lectures", lecture)
-
-        .catch(err => {
-            console.error(err)
-        })
-
+            .catch(err => {
+                console.error(err)
+            })
     }
 
+    /**
+     * 
+     * @param {String} videoId
+     * @param {blobClient} blob 
+     */
     static deleteVideo(videoId, blob) {
         TypeChecker.isString(videoId);
+        TypeChecker.isInstanceOf(blob, blobClient);
+
         return blob.deleteFile("lectures", videoId);
     }
 
+    /**
+     * 
+     * @param {db} vimsudb 
+     */
     static deleteAllSlots(vimsudb) {
+        TypeChecker.isInstanceOf(vimsudb, db);
+
         return vimsudb.deleteAllFromCollection("lectures").then(res => {
             console.log("all slots deleted");
         }).catch(err => {
@@ -64,7 +104,15 @@ module.exports = class SlotService {
         })
     }
 
+    /**
+     * 
+     * @param {String} lectureId 
+     * @param {db} vimsudb 
+     */
     static deleteSlot(lectureId, vimsudb) {
+        TypeChecker.isString(lectureId);
+        TypeChecker.isInstanceOf(vimsudb, db);
+
         return vimsudb.deleteOneFromCollection("lectures", { lectureId: lectureId }).then(res => {
             console.log("slot with lectureId " + lectureId + " deleted");
         }).catch(err => {
