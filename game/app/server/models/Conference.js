4fe38e5b0f22e8bd18d1a774690c38a6e15fd230
klaudialeo <klaudialeo@yahoo.com>
2020-08-26 10:02:08 +0200

    add JSDoc and typechecker of Conference

--- game/app/server/models/Conference.js
+++ game/app/server/models/Conference.js
@@ -1,8 +1,16 @@
+const TypeChecker = require("../../client/shared/TypeChecker");
+const Schedule = require('./Schedule');
+
 module.exports = class Conference {
 
     #schedule;
 
+    /**
+     * 
+     * @param {Schedule} schedule 
+     */
     constructor(schedule) {
+        TypeChecker.isInstanceOf(schedule, Schedule);
         this.#schedule = schedule;
     }
 
