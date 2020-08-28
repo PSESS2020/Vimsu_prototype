04670bae516a42fea62cde4ae5abb62a375dbd7a
klaudialeo <klaudialeo@yahoo.com>
2020-08-28 19:26:47 +0200

    made gameLoop private

--- src/game/app/client/index.js
+++ src/game/app/client/index.js
@@ -7,50 +7,56 @@
 
 const GAME_WIDTH = GameConfig.CTX_WIDTH;
 const GAME_HEIGHT = GameConfig.CTX_HEIGHT;
-let ctx_map, ctx_avatar, ctx_ui;
-let clientController;
+var ctx_map, ctx_avatar, ctx_ui;
+var clientController;
 
 /* Initializes canvas and ClientController on window onload */
-window.onload = this.init();
+window.onload = new index().init();
 
 /**
- * Initializes canvas and ClientController for the first time.
- * Also calls game loop to update frame constantly
+ * Holds init() and gameLoop()
  */
-function init() {
-  ctx_map = document.getElementById("mapCanvas").getContext("2d");
-  ctx_avatar = document.getElementById("avatarCanvas").getContext("2d");
-  ctx_ui = document.getElementById("uiCanvas").getContext("2d");
+function index() {
 
-  ctx_map.canvas.width = GAME_WIDTH;
-  ctx_map.canvas.height = GAME_HEIGHT;
+  /**
+   * Initializes canvas and ClientController for the first time.
+   * Also calls game loop to update frame constantly
+   */
+  this.init = function() {
+    ctx_map = document.getElementById("mapCanvas").getContext("2d");
+    ctx_avatar = document.getElementById("avatarCanvas").getContext("2d");
+    ctx_ui = document.getElementById("uiCanvas").getContext("2d");
+  
+    ctx_map.canvas.width = GAME_WIDTH;
+    ctx_map.canvas.height = GAME_HEIGHT;
+  
+    ctx_avatar.canvas.width = GAME_WIDTH;
+    ctx_avatar.canvas.height = GAME_HEIGHT;
+  
+    ctx_ui.canvas.width = GAME_WIDTH;
+    ctx_ui.canvas.height = GAME_HEIGHT;
+  
+    clientController = new ClientController(GameConfig.PORT);
+  
+    // Start the first frame request
+    window.requestAnimationFrame(gameLoop);
+  }
 
-  ctx_avatar.canvas.width = GAME_WIDTH;
-  ctx_avatar.canvas.height = GAME_HEIGHT;
+  /**
+   * @private Updates game constantly through a game loop
+   * 
+   * @param {number} timeStamp 
+   */
+  var gameLoop = function(timeStamp) {
+    TypeChecker.isNumber(timeStamp);
 
-  ctx_ui.canvas.width = GAME_WIDTH;
-  ctx_ui.canvas.height = GAME_HEIGHT;
-
-  clientController = new ClientController(GameConfig.PORT);
-
-  // Start the first frame request
-  window.requestAnimationFrame(gameLoop);
-}
-
-/**
- * Updates game constantly through a game loop
- * 
- * @param {number} timeStamp 
- */
-function gameLoop(timeStamp) {
-  TypeChecker.isNumber(timeStamp);
-
-  //Clear canvas
-  ctx_avatar.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
-
-  // Perform the drawing operation
-  clientController.updateGame(timeStamp);
-
-  // The loop function has reached it's end. Keep requesting new frames
-  window.requestAnimationFrame(gameLoop);
+    // Clear canvas
+    ctx_avatar.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+  
+    // Perform the drawing operation
+    clientController.updateGame(timeStamp);
+  
+    // The loop function has reached it's end. Keep requesting new frames
+    window.requestAnimationFrame(gameLoop);
+  }
 }
