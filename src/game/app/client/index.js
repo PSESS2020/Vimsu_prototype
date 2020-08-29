e68fc5074a5e624c8c5e2e6961cbdc5e57ff9f75
klaudialeo <klaudialeo@yahoo.com>
2020-08-29 21:15:47 +0200

    remove ui canvas because its not used

--- src/game/app/client/index.js
+++ src/game/app/client/index.js
@@ -5,7 +5,10 @@
  * @version 1.0.0
  */
 
-var ctx_map, ctx_avatar, ctx_ui;
+/**
+ * canvas variable
+ */
+var ctx_map, ctx_avatar;
 
 /* Initializes canvas and ClientController on window onload */
 window.onload = new index().init();
@@ -27,16 +30,12 @@ function index() {
   this.init = function() {
     ctx_map = document.getElementById("mapCanvas").getContext("2d");
     ctx_avatar = document.getElementById("avatarCanvas").getContext("2d");
-    ctx_ui = document.getElementById("uiCanvas").getContext("2d");
   
     ctx_map.canvas.width = GAME_WIDTH;
     ctx_map.canvas.height = GAME_HEIGHT;
   
     ctx_avatar.canvas.width = GAME_WIDTH;
     ctx_avatar.canvas.height = GAME_HEIGHT;
-  
-    ctx_ui.canvas.width = GAME_WIDTH;
-    ctx_ui.canvas.height = GAME_HEIGHT;
 
     var gameView = new GameView();
     clientController = new ClientController(GameConfig.PORT, gameView);
