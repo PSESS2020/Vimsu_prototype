bcd69a346d25b63f4fed8e5a01bb582756300b9c
Eric Ritte <upebw@student.kit.edu>
2021-03-07 12:07:30 +0100

    initialized some changes

---
+++ src/game/app/server/utils/Floorplan.js
@@ -0,0 +1,33 @@
+const Direction = require('../../client/shared/Direction');
+const Globals = require('./GlobalStrings.js');
+
+/**
+ * How to use this file:
+ * yadada
+ * @module Floorplan
+ * 
+ * @author Eric Ritte, Klaudia Leo, Laura Traub, Niklas Schmidt, Philipp Schumacher
+ * @version 1.0.0
+ */
+module.exports = Object.freeze({
+
+    /*
+    EXAMPLE: {
+
+    }
+    */
+
+    ROOM1: {
+        Type: Globals.RECEPTION,
+        SIZE_X: 100,
+        SIZE_Y: 100,
+        START_X: 0,
+        START_Y: 0,
+        DOORS: [],
+        OBJECTS: [],
+
+    }
+    
+
+
+})
