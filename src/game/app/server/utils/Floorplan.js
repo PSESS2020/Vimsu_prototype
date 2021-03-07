7347634dd026ce0acd05dd50775e171b8910f6da
Eric Ritte <upebw@student.kit.edu>
2021-03-07 13:17:13 +0100

    started mapping out the necessary changes to allow for easier room creation

--- src/game/app/server/utils/Floorplan.js
+++ src/game/app/server/utils/Floorplan.js
@@ -17,17 +17,39 @@ module.exports = Object.freeze({
     }
     */
 
+    // ROOM0
+    // This will be the starting room
+
+    // Maybe offer "blueprints" for certain types
+    // of rooms or even conferences?
+
+    // There should be an external file where all
+    // the necessary strings are saved...
+    // (this can just be the globalStrings-file)
+    // (also asset-paths file)
+
+    // Required
+    // - Objects
+    // - Map Elements (Walls & Windows)
+    // - NPCs
+    // - Doors
+    // Best way of doing doors is to force people to manually set ids of rooms and then give exit-room id when listing doors
+
     ROOM1: {
-        Type: Globals.RECEPTION,
-        SIZE_X: 100,
-        SIZE_Y: 100,
-        START_X: 0,
-        START_Y: 0,
+        TYPE: Globals.RECEPTION,
+        WIDTH: 100,
+        LENGTH: 100,
+        SHAPE: 'L',
+        // Entry Points
+        // Start position for reception?
+        WALLTYPE: 'default',
+        TILETYPE: 'default',
+        MAPELEMENTS: [],
         DOORS: [],
         OBJECTS: [],
-
+        NPCS: []
     }
-    
+
 
 
 })
