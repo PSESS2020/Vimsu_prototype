c9a9eb36baefbf7ec4cf475059317752e727d819
klaudialeo <klaudialeo@yahoo.com>
2021-03-14 13:52:46 +0100

    fix npc story modal bug

--- src/game/app/client/views/js/NPCStoryView.js
+++ src/game/app/client/views/js/NPCStoryView.js
@@ -6,8 +6,6 @@
  */
 class NPCStoryView extends WindowView {
 
-    #npcId;
-
     /**
      * Creates an instance of NPCStoryView
      */
@@ -26,65 +24,69 @@ class NPCStoryView extends WindowView {
      * 
      * @param {String} name NPC name
      * @param {String[]} story NPC story
+     * @param {String} npcId NPC id
      */
-    draw(name, story) {
-        $('#npcStoryWait' + this.#npcId).hide()
-        $(`#npcStoryModal${this.#npcId} .modal-header`).empty()
-        $('#npcStory' + this.#npcId).empty();
+    draw(name, story, npcId) {
+        $('#npcStoryWait' + npcId).hide()
+        $(`#npcStoryModal${npcId} .modal-header`).empty()
+        $('#npcStory' + npcId).empty();
         
-        $(`#npcStoryModal${this.#npcId} .modal-header`).append(`
-            <h5 class="modal-title d-inline-block" id="npcStoryTitle">${name + " says..."}</h5>
+        $(`#npcStoryModal${npcId} .modal-header`).append(`
+            <h5 class="modal-title d-inline-block" id="npcStoryTitle${npcId}">${name + " says..."}</h5>
             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
         `)
 
         for (var i = 0; i < story.length; i++) {
-            $('#npcStory' + this.#npcId).append(`
-                <h5 style="background-color: rgba(0, 0, 0, 0); padding: 5px; text-align: left; display:none" id='${"story" + i.toString()}'>${story[i]}</h5>
-                <button style="float:left; display: none; outline: none; box-shadow: none" class="btn" id='${"backwardStory" + i.toString()}'>
+            $('#npcStory' + npcId).append(`
+                <h5 style="background-color: rgba(0, 0, 0, 0); padding: 5px; text-align: left; display:none" id='${"story" + npcId + i.toString()}'>${story[i]}</h5>
+                <button style="float:left; display: none; outline: none; box-shadow: none" class="btn" id='${"backwardStory" + npcId + i.toString()}'>
                     <i class="fa fa-arrow-left fa-3x navbarIcons"></i>
                 </button>
-                <button style="float:right; display: none; outline: none; box-shadow: none" class="btn" id='${"forwardStory" + i.toString()}'>
+                <button style="float:right; display: none; outline: none; box-shadow: none" class="btn" id='${"forwardStory" + npcId + i.toString()}'>
                     <i class="fa fa-arrow-right fa-3x navbarIcons"></i>
                 </button>
 
                 <script>
-                    $("#backwardStory" + '${i}').on('click', function(event) {
-                        $('#story' + '${i}').hide();
-                        $('#backwardStory' + '${i}').hide();
-                        $('#forwardStory' + '${i}').hide();
-                        $('#story' + '${i - 1}').show();
-                        $('#forwardStory' + '${i - 1}').show();
+                    $("#backwardStory${npcId}${i}").on('click', function(event) {
+                        $('#story${npcId}${i}').hide();
+                        $('#backwardStory${npcId}${i}').hide();
+                        $('#forwardStory${npcId}${i}').hide();
+                        $('#story${npcId}${i - 1}').show();
+                        $('#forwardStory${npcId}${i - 1}').show();
 
                         if(!${i === 1}) {
-                            $('#backwardStory' + '${i - 1}').show();
+                            $('#backwardStory${npcId}${i - 1}').show();
                         }
                     })    
-                    $("#forwardStory" + '${i}').on('click', function(event) {
-                        $('#story' + '${i}').hide();
-                        $('#backwardStory' + '${i}').hide();
-                        $('#forwardStory' + '${i}').hide();
-                        $('#story' + '${i + 1}').show();
-                        $('#backwardStory' + '${i + 1}').show();
+                    $("#forwardStory${npcId}${i}").on('click', function(event) {
+                        $('#story${npcId}${i}').hide();
+                        $('#backwardStory${npcId}${i}').hide();
+                        $('#forwardStory${npcId}${i}').hide();
+                        $('#story${npcId}${i + 1}').show();
+                        $('#backwardStory${npcId}${i + 1}').show();
 
                         if(!${i === story.length - 2}) {
-                            $('#forwardStory' + '${i + 1}').show();
+                            $('#forwardStory${npcId}${i + 1}').show();
                         }
                     }) 
                 </script>
             `);
 
             if (i === 0) {
-                $('#story' + i).show();
-                $('#forwardStory' + i).show();
+                $('#story' + npcId + i).show();
+                $('#forwardStory' + npcId + i).show();
             };
         }
     }
 
+    /**
+     * Add new NPC story window
+     * 
+     * @param {String} npcId NPC id
+     */
     addNewNPCStoryWindow(npcId) {
-        this.#npcId = npcId;
-
         if (!($('#npcStoryModal' + npcId).length)) {
             $('#npcStoryModalCollection').append(`
                 <div class="modal fade" id="npcStoryModal${npcId}" tabindex="-1" role="dialog" aria-labelledby="npcStoryTitle${npcId}"
