08eba3c376f0cd16d08d906ceb34040b36150760
pnschumacher <philipp.schumacher@gmx.de>
2021-03-24 18:47:49 +0100

    Added fullscreen button to ExternalWebsiteWindows. Does not work as expected yet

--- src/game/app/client/views/js/ExternalWebsiteView.js
+++ src/game/app/client/views/js/ExternalWebsiteView.js
@@ -33,16 +33,58 @@
         $(`#externalWebsiteWaitModal${gameObjectID} .modal-header`).empty()
         $('#externalWebsiteBody' + gameObjectID).empty();
 
+        let fullScreenMode = false;
         let width = iFrameData.width.toString() + 'px';
         let height = iFrameData.height.toString() + 'px';
 
         $('#externalWebsiteBody' + gameObjectID).append(`
             <div class="modal-body" style="overflow:auto; height:${height};">
                 <p style="text-align:center">
-                    <iframe id="iframe" frameborder="1" src=${iFrameData.url} width=${width} height=${height} allowfullscreen scrolling="no"></iframe>
+                    <iframe id=${"iframe" + gameObjectID} frameborder="1" src=${iFrameData.url} width=${width} height=${height} 
+                        allowfullscreen scrolling="no" sandbox="allow-same-origin allow-scripts allow-popups allow-forms"></iframe>
                 </p>
             </div>
         `);
+
+        $('#fullscreenBtn' + gameObjectID).off();
+        $('#fullscreenBtn' + gameObjectID).on('click', (event) => {
+            event.preventDefault();
+
+            let iFrame = document.getElementById("iframe" + gameObjectID);
+            let window = document.getElementById("externalWebsiteModal" + gameObjectID);
+
+            if (!fullScreenMode) {
+            
+                iFrame.width = '100%';
+                iFrame.height = '100%';
+                iFrame.scrolling= 'yes';
+
+                if (window.requestFullscreen) {
+                    window.requestFullscreen();
+                } else if (window.webkitRequestFullscreen) { /* Safari */
+                    window.webkitRequestFullscreen();
+                } else if (window.msRequestFullscreen) { /* IE11 */
+                    window.msRequestFullscreen();
+                }
+
+                fullScreenMode = true;
+            } else {
+        
+                iFrame.width = iFrameData.width;
+                iFrame.height = iFrameData.height;
+                iFrame.scrolling= 'no';
+
+                if (document.exitFullscreen) {
+                    document.exitFullscreen();
+                } else if (document.webkitExitFullscreen) { /* Safari */
+                    document.webkitExitFullscreen();
+                } else if (document.msExitFullscreen) { /* IE11 */
+                    document.msExitFullscreen();
+                }
+
+                fullScreenMode = false;
+            }
+        });
      }
     
     /**
@@ -57,9 +99,19 @@
                     <div class="modal-dialog modal-dialog-centered mw-100 w-75" role="document">
                         <div class="modal-content">
                             <div class="modal-header">
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
+                                <h5 class="modal-title" id=${"externalWebsiteModalTitle" + gameObjectID}></h5>
+                                <div class="d-flex flex-row justify-content-end">
+                                    <div>
+                                        <button id=${"fullscreenBtn" + gameObjectID} class="close btn">
+                                            <i class="fa fa-window-maximize" style=" transform: scale(0.8); margin-top: 1px;"></i>
+                                        </button>
+                                    </div>
+                                    <div>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                </div>
                             </div>
                             <div class="modal-body">
                                 <div id="externalWebsiteWait${gameObjectID}" style="text-align: center;">
