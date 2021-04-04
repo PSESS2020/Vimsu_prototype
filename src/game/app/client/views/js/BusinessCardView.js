305b25396460f58585eb76ce9dc7579af78bc8d3
klaudialeo <klaudialeo@yahoo.com>
2021-04-04 19:59:31 +0200

    refactoring

--- src/game/app/client/views/js/BusinessCardView.js
+++ src/game/app/client/views/js/BusinessCardView.js
@@ -50,50 +50,26 @@ class BusinessCardView extends WindowView {
 
         $('#businessCardModal .modal-body').append(`
                 
-                <h5 style="background-color: rgba(24, 30, 32, 0.699); padding: 5px; text-align: center">
-                <i class="fa fa-user-circle pr-2 navbarIcons" style="transform: scale(1)"></i>
-                ${this.businessCard.getTitle() + " " + this.businessCard.getForename() + " " + this.businessCard.getSurname() + " (@" + this.businessCard.getUsername() + ")"}</h5>
-                </br>
-                <table id="${"profile" + this.businessCard.getParticipantId()}" style = "color: antiquewhite; width:100%; margin-left: 0">
-                    <tr>
-                        <td style="border-right: 1pt solid antiquewhite; text-align: right; padding: 15px" >Profession</td>
-                        <td style="padding: 15px">${this.businessCard.getJob() + " at " + this.businessCard.getCompany()}</td>
-                    </tr>
-        `)
-
-        if (this.isFriend) {
-            $('#businessCardModal .modal-body #profile' + this.businessCard.getParticipantId()).append(`
+            <h5 style="background-color: rgba(24, 30, 32, 0.699); padding: 5px; text-align: center">
+            <i class="fa fa-user-circle pr-2 navbarIcons" style="transform: scale(1)"></i>
+            ${this.businessCard.getTitle() + " " + this.businessCard.getForename() + " " + this.businessCard.getSurname() + " (@" + this.businessCard.getUsername() + ")"}</h5>
+            </br>
+            <table id="${"profile" + this.businessCard.getParticipantId()}" style = "color: antiquewhite; width:100%; margin-left: 0">
                 <tr>
-                    <td style="border-right: 1pt solid antiquewhite ; text-align: right; padding: 15px">Email</td>
-                    <td style="padding: 15px">${this.businessCard.getEmail()}</td>
+                    <td style="border-right: 1pt solid antiquewhite; text-align: right; padding: 15px" >Profession</td>
+                    <td style="padding: 15px">${this.businessCard.getJob() + " at " + this.businessCard.getCompany()}</td>
                 </tr>
-            `)
-        } else if (!this.isFriend && !this.isModerator) {
-            $('#businessCardModal .modal-body #profile' + this.businessCard.getParticipantId()).append(`
-                <tr>
-                    <td style="border-right: 1pt solid antiquewhite ; text-align: right; padding: 15px">Rank</td>
-                    <td style="padding: 15px">${this.rank}</td>
-                </tr>
-            `)
-        }
-
-        if (this.isModerator) {
-            $('#businessCardModal .modal-body #profile' + this.businessCard.getParticipantId()).append(`
+                ${this.isFriend || !this.isModerator ?
+                `<tr>
+                    <td style="border-right: 1pt solid antiquewhite ; text-align: right; padding: 15px">${this.isFriend ? "Email" : "Rank"}</td>
+                    <td style="padding: 15px">${this.isFriend ? this.businessCard.getEmail() : this.rank}</td>
+                </tr>`
+                : ``
+                }
                 <tr>
                     <td style="border-right: 1pt solid antiquewhite ; text-align: right; padding: 15px">Role</td>
-                    <td style="padding: 15px">Moderator</td>
+                    <td style="padding: 15px">${this.isModerator ? "Moderator" : "Participant"}</td>
                 </tr>
-            `)
-        } else {
-            $('#businessCardModal .modal-body #profile' + this.businessCard.getParticipantId()).append(`
-                <tr>
-                    <td style="border-right: 1pt solid antiquewhite ; text-align: right; padding: 15px">Role</td>
-                    <td style="padding: 15px">Participant</td>
-                </tr>
-            `)
-        }
-
-        $('#businessCardModal .modal-body').append(`
             </table>
             </br>
             <button id="${"chatnow" + this.businessCard.getParticipantId()}" title ="Close business card and chat now" class="btn btn-blue mx-auto d-block">Chat</button>
--- src/game/app/client/views/js/ProfileView.js
+++ src/game/app/client/views/js/ProfileView.js
@@ -55,29 +55,12 @@ class ProfileView extends WindowView {
                         <td style="border-right: 1pt solid antiquewhite ; text-align: right; padding: 15px">Email</td>
                         <td style="padding: 15px">${this.businessCard.getEmail()}</td>
                     </tr>
-        `)
-
-        if(this.isModerator) {
-            $('#profileModal .modal-body #profile').append(`
                     <tr>
                         <td style="border-right: 1pt solid antiquewhite ; text-align: right; padding: 15px">Role</td>
-                        <td style="padding: 15px">Moderator</td>
+                        <td style="padding: 15px">${this.isModerator ? "Moderator" : "Participant"}</td>
                     </tr>
-            `)
-        } else {
-            $('#profileModal .modal-body #profile').append(`
-                    <tr>
-                        <td style="border-right: 1pt solid antiquewhite ; text-align: right; padding: 15px">Role</td>
-                        <td style="padding: 15px">Participant</td>
-                    </tr>
-            `)
-        }
-
-        $('#profileModal .modal-body').append(`
                 </table>
             </div>
         `)
-
-        
     }
 }
