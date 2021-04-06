67dca34a27ad304a6578d70caf130cb81d582a45
Eric Ritte <upebw@student.kit.edu>
2021-04-06 15:01:06 +0200

    added shared assetPath-file

---
+++ src/game/app/client/shared/AssetPaths.js
@@ -0,0 +1,83 @@
+/**
+ * @enum The paths of the images corresponding to the different assets
+ * 
+ * @author Eric Ritte, Klaudia Leo, Laura Traub, Niklas Schmidt, Philipp Schumacher
+ * @version 1.0.0
+ */
+ const AssetPaths = Object.freeze({
+    BLUE: "Blue",
+    RED: "Red",
+    GREEN: "Green",
+    YELLOW: "Yellow",
+    WHITE: "White"
+
+});
+
+if (typeof module === 'object' && typeof exports === 'object') {
+    module.exports = AssetPaths;
+}
+
+/* And it looks a little something like this
+
+    // FOODCOURT:
+
+        "tile_default": "client/assets/tiles/tile_default.png",
+
+        "leftwall_default": "client/assets/walls/wall1.png",
+
+        "rightwall_default": "client/assets/walls/wall2.png",
+
+        "leftfoyerdoor_default": "client/assets/doors/door_foyer.png",
+
+        "rightreceptiondoor_default": "client/assets/doors/door_reception.png",
+
+        "rightwindow_default0": "client/assets/windows/right_small_window_default0.png",
+
+        "leftconferencelogo_default0": "client/assets/logos/conferencelogo1.png",
+
+        "leftconferencelogo_default1": "client/assets/logos/conferencelogo2.png",
+
+        "leftconferencelogo_default2": "client/assets/logos/conferencelogo3.png",
+        "leftconferencelogo_default3": "client/assets/logos/conferencelogo4.png",
+        "leftconferencelogo_default4": "client/assets/logos/conferencelogo5.png",
+
+        "righttable_default": "client/assets/tables/dinnerTableRight.png",
+        "leftchair_default": "client/assets/chairs/chair_left.png",
+        "rightchair_default": "client/assets/chairs/chair_right.png",
+        "leftchairback_default": "client/assets/chairs/chair_left_back.png",
+        "rightchairback_default": "client/assets/chairs/chair_right_back.png",
+        "smalldinnertable_default": "client/assets/tables/smallDinnerTable.png",
+        "canteencounter_default": "client/assets/other/canteenCounter.png",
+        "drinks_default": "client/assets/other/Drinks.png",
+        "koeriWurst_bothSides": "client/assets/food/koeriWurscht_bothSides.png",
+        "koeriWurst_upperSide": "client/assets/food/koeriWurscht_upperSide.png",
+        "koeriWurst_lowerSide": "client/assets/food/koeriWurscht_lowerSide.png",
+        "tea_default": "client/assets/food/tea.png",
+
+
+    // FOYER
+
+         "tile_default": "client/assets/tiles/tile_default.png",
+        "leftwall_default": "client/assets/walls/wall1.png",
+        "rightwall_default": "client/assets/walls/wall2.png",
+        "leftlecturedoor_default": "client/assets/doors/door_lecturehall.png",
+        "rightfoodcourtdoor_default": "client/assets/doors/door_foodcourt.png",
+        "rightreceptiondoor_default": "client/assets/doors/door_reception.png",
+        "leftschedule_default0": "client/assets/walls/schedule1.png",
+        "leftschedule_default1": "client/assets/walls/schedule2.png",
+        "leftschedule_default2": "client/assets/walls/schedule3.png",
+        "leftwindow_default0": "client/assets/windows/left_small_window_default0.png",
+        "rightwindow_default0": "client/assets/windows/right_small_window_default0.png",
+        "leftconferencelogo_default0": "client/assets/logos/conferencelogo1.png",
+        "leftconferencelogo_default1": "client/assets/logos/conferencelogo2.png",
+        "leftconferencelogo_default2": "client/assets/logos/conferencelogo3.png",
+        "leftconferencelogo_default3": "client/assets/logos/conferencelogo4.png",
+        "leftconferencelogo_default4": "client/assets/logos/conferencelogo5.png",
+        "rightwallframe_default0": "client/assets/frames/wallframe1.png",
+        "rightwallframe_default1": "client/assets/frames/wallframe2.png",
+        "rightwallframe_default2": "client/assets/frames/wallframe3.png",
+        "leftsofa_default": "client/assets/chairs/sofa_left.png",
+        "rightsofa_default": "client/assets/chairs/sofa_right.png",
+        "plant_default": "client/assets/plants/plant.png"
+
+    */
