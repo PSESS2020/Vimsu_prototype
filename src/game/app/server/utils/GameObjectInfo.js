e316c0af3f7f4bf01f61a9777f9915986477e6b8
Eric Ritte <upebw@student.kit.edu>
2021-04-07 16:02:06 +0200

    Further work on the GameObjectInfo-class. Made it a bit more useable and added instructions on how to add your own object.

--- src/game/app/server/utils/GameObjectInfo.js
+++ src/game/app/server/utils/GameObjectInfo.js
@@ -1,8 +1,7 @@
 const GameObjectType = require("../../client/shared/GameObjectType");
+const TypeChecker = require("../../client/shared/TypeChecker");
 
 /**
- * @enum GameObjectInformation
- * 
  * Indexed by the contents of the GameObjectType-file,
  * this file makes the information need to generate
  * an object from just its type easily accessible.
@@ -13,10 +12,35 @@ const GameObjectType = require("../../client/shared/GameObjectType");
  * would break several client-side classes, and I can't be bothered
  * to fix them all.
  * 
+ * Done as a static class for reasons of privacy. It is just nicer to
+ * call a function instead of nested calls of object properties.
+ * 
+ * 
  * @author Eric Ritte, Klaudia Leo, Laura Traub, Niklas Schmidt, Philipp Schumacher
  * @version 1.0.0
  */
-const GameObjectInfo = Object.freeze({
+class GameObjectInfo {
+    /**********************************************************/
+    /** When adding the information for a new GameObjectType **/
+    /** file, please stick to the following instructions:    **/
+    /**                                                      **/
+    /**   (a) Add the  needed information as a  new entry to **/ 
+    /**       #INFORMATION-field. You don't need to stick to **/
+    /**       the categories,  but it helps keeping the file **/
+    /**       more clearly structured.                       **/
+    /**                                                      **/
+    /**   (b) Add a new property  to the #INFORMATION-field. **/
+    /**       It should be formatted like this:              **/
+    /**       [GameObjectType.<property_key>]: {},           **/
+    /**                  (key)               (value)         **/
+    /**       The brackets [] around the key are mandatory!  **/
+    /**       Replace  <property_key>  with the  KEY of the  **/
+    /**       property you just added to the GameObjectType. **/
+    /**                                                      **/
+    /**   (c) For the value of the just added*/
+    /**********************************************************/
+
+
     // Necessary information for each to add:
     // - isSolid
     // - size
@@ -24,37 +48,68 @@ const GameObjectInfo = Object.freeze({
     // - is there an easy way to group similar objects
     //   as variations of the same type?
 
-    // Blank
-    [GameObjectType.BLANK]: {},
-    // Tiles
-    [GameObjectType.SELECTED_TILE]: {},
-    [GameObjectType.TILE]: {},
-    [GameObjectType.LEFTTILE]: {},
-    [GameObjectType.RIGHTTILE]: {},
-    // Walls
-    [GameObjectType.LEFTWALL]: {},
-    [GameObjectType.RIGHTWALL]: {},
-    // Schedule
-    [GameObjectType.LEFTSCHEDULE]: {},
-    // Plant
-    [GameObjectType.PLANT]: {},
-    // Logo
-    [GameObjectType.CONFERENCELOGO]: {},
-    // Seating
-    [GameObjectType.CHAIR]: {},
-    [GameObjectType.SOFA]: {},
-    // Tables
-    [GameObjectType.TABLE]: {},
-    [GameObjectType.RIGHTTABLE]: {},
-    [GameObjectType.SMALLDINNERTABLE]: {},
-    // Counters
-    [GameObjectType.CANTEENCOUNTER]: {},
-    [GameObjectType.RECEPTIONCOUNTER]: {},
-    [GameObjectType.RECEPTIONCOUNTERSIDEPART]: {},
-    // Food & Drinks
-    [GameObjectType.DRINKS]: {},
-    [GameObjectType.SMALLDINNERTABLEFOOD]: {},
-});
+    /**
+     * Takes a GameObjectType and a <key> as arguments and checks
+     * if a property with the name <key> is stored for the passed
+     * GameObjectType. If yes, it is returned. If no, throws error.
+     * 
+     * @method moduke:GameObjectInfo#getInfo
+     * 
+     * @param {String} objectType 
+     * @param {String} key 
+     * @returns {*} The information saved for the passed GameObjectType
+     *              under the passed key (if it exists).
+     */
+    static getInfo(objectType, key) {
+        TypeChecker.isEnumOf(objectType, GameObjectType);
+        if (GameObjectInfo.#INFORMATION[objectType].hasOwnProperty(key)) {
+            return GameObjectInfo.#INFORMATION[objectType][key];
+        } else {
+            throw new Error("The passed GameObjectType " + objectType + " does not have the property " + key);
+        }
+    }
+
+    /**
+     * All the info for each GameObjectType
+     * 
+     * 
+     */
+    static #INFORMATION = Object.freeze({
+        // Blank
+        [GameObjectType.BLANK]: {},
+        // Tiles
+        [GameObjectType.SELECTED_TILE]: {},
+        [GameObjectType.TILE]: {},
+        [GameObjectType.LEFTTILE]: {},
+        [GameObjectType.RIGHTTILE]: {},
+        // Walls
+        [GameObjectType.LEFTWALL]: {},
+        [GameObjectType.RIGHTWALL]: {},
+        // Schedule
+        [GameObjectType.LEFTSCHEDULE]: {},
+        // Windows
+        [GameObjectType.WINDOW]: {},
+        // Plant
+        [GameObjectType.PLANT]: {},
+        // Logo
+        [GameObjectType.CONFERENCELOGO]: {},
+        // Seating
+        [GameObjectType.CHAIR]: {},
+        [GameObjectType.SOFA]: {},
+        // Tables
+        [GameObjectType.TABLE]: {},
+        [GameObjectType.RIGHTTABLE]: {},
+        [GameObjectType.SMALLDINNERTABLE]: {},
+        // Counters
+        [GameObjectType.CANTEENCOUNTER]: {},
+        [GameObjectType.RECEPTIONCOUNTER]: {},
+        [GameObjectType.RECEPTIONCOUNTERSIDEPART]: {},
+        // Food & Drinks
+        [GameObjectType.DRINKS]: {},
+        [GameObjectType.SMALLDINNERTABLEFOOD]: {},
+    });
+    
+}
 
 if (typeof module === 'object' && typeof exports === 'object') {
     module.exports = GameObjectInfo;
