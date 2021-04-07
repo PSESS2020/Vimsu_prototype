599f99106341638d38afd8d40af962227c0ff0a8
klaudialeo <klaudialeo@yahoo.com>
2021-04-07 20:16:50 +0200

    modal style fix

--- src/game/app/client/views/js/MeetingListView.js
+++ src/game/app/client/views/js/MeetingListView.js
@@ -89,7 +89,7 @@ class MeetingListView extends WindowView {
             <li class="list-group-item bg-transparent chatthread" id="${"meetingEntry" + meeting.id}">
                 <a class="" style="color: antiquewhite" title="Open meeting" id="${"meeting" + meeting.id}" role="button" data-toggle="modal" href="">
                         <div class="row w-100">
-                            <div class="col-12 col-sm-2 px-0">
+                            <div class="col-2 px-0">
                                 <i class="fa fa-video fa-5x navbarIcons" style="transform: scale(0.7)" ></i>
                             </div>
                             <div class="col-10 text-left">
