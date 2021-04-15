53f93b1d4d49f70d4a9daf0a18f4fef03421b226
klaudialeo <klaudialeo@yahoo.com>
2021-04-15 12:32:44 +0200

    reconnect on losing internet connection

--- src/game/app/client/controller/ClientController.js
+++ src/game/app/client/controller/ClientController.js
@@ -144,10 +144,11 @@ class ClientController {
 
             this.socket.on('disconnect', (reason) => {
                 this.gameView.updateConnectionStatus(ConnectionState.DISCONNECTED);
-                if (reason !== "transport close") {
-                    this.socket.close();
-                } else {
+                
+                if (reason === "transport close" || reason === "ping timeout" || reason === "transport error") {
                     window.setTimeout(() => { this.openSocketConnection() }, 5000 );
+                } else {
+                    this.socket.close();
                 }
             });
 
