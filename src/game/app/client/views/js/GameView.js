4d92255a983c3a925c185a9ec7f6bc5f3e21fd67
pnschumacher <philipp.schumacher@gmx.de>
2021-04-20 14:18:04 +0200

    Fix merging error

--- src/game/app/client/views/js/GameView.js
+++ src/game/app/client/views/js/GameView.js
@@ -457,7 +457,6 @@ class GameView {
             this.npcAvatarViews.push(new NPCAvatarView(npc.getId(), npc.getName(), npc.getPosition(), npc.getDirection(), npc.getShirtColor(), this.gameEngine, this.eventManager));
         });
 
-        this.currentMapView = new MapView(assetPaths, map, objectMap, this.gameEngine, this.eventManager);
         this.statusBar.updateLocation(roomName);
     }
 
