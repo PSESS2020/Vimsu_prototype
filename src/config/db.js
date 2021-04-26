34e61404605fff5cce4a210a83ef4211fec555a6
klaudialeo <klaudialeo@yahoo.com>
2021-04-26 23:06:25 +0200

    minor

--- src/config/db.js
+++ src/config/db.js
@@ -96,7 +96,7 @@ module.exports = class db {
      * 
      * @return {Object[]} documents
      */
-    findAllInCollection(collectionName) {
+    findAllInCollection(collectionName, sortedBy) {
         TypeChecker.isString(collectionName);
         var collection = this.#vimsudb.collection(collectionName);
 
