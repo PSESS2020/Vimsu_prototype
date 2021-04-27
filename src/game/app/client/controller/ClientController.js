672e8b4b583f41c9730c4937782e9419c9234aa5
klaudialeo <klaudialeo@yahoo.com>
2021-04-27 21:02:16 +0200

    fix viewblocker doesn't show up when user is banned

--- src/game/app/client/controller/ClientController.js
+++ src/game/app/client/controller/ClientController.js
@@ -984,6 +984,7 @@ class ClientController {
      * Receives from server that client has been removed from the conference
      */
     handleFromServerRemoved = function () {
+        $("#windowHTML").show();
         $('#viewBlocker').show();
     };
 
